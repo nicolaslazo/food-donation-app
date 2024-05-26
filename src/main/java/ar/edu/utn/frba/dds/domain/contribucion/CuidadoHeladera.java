3c52b19d33083536ea28f85723c94429f983f3e2
nicolaslazo <45973144+nicolaslazo@users.noreply.github.com>
2024-05-26 17:47:10 -0300

    CuidadoHeladera sólo registra una una sola heladera

--- src/main/java/ar/edu/utn/frba/dds/domain/contribucion/CuidadoHeladera.java
+++ src/main/java/ar/edu/utn/frba/dds/domain/contribucion/CuidadoHeladera.java
@@ -6,19 +6,17 @@ import lombok.Getter;
 import lombok.NonNull;
 
 import java.time.ZonedDateTime;
-import java.util.List;
+
 public class CuidadoHeladera extends Contribucion {
         private ZonedDateTime fechaContribucion;
 
-        //TODO
-        //SE HACE CARGO DE VARIAS HELADERAS A LA VEZ? O SOLO UNA POR CONTRIBUCION?
         @Getter
-        private List<Heladera> heladeras;
+        private Heladera heladera;
 
-        public CuidadoHeladera(@NonNull Colaborador colaborador, @NonNull ZonedDateTime fecha, ZonedDateTime fechaContribucion, List<Heladera> heladeras) {
+        public CuidadoHeladera(@NonNull Colaborador colaborador, @NonNull ZonedDateTime fecha, ZonedDateTime fechaContribucion, Heladera heladera) {
                 super(colaborador, fecha);
                 this.fechaContribucion = fechaContribucion;
-                this.heladeras = heladeras;
+                this.heladera = heladera;
         }
 
 }
