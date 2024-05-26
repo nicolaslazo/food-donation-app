ac9a849a9b1d6cee88835976d29c1e770c1b7385
nicolaslazo <45973144+nicolaslazo@users.noreply.github.com>
2024-05-26 20:46:15 -0300

    Estado de heladera implementado

---
+++ src/main/java/ar/edu/utn/frba/dds/domain/heladera/Heladera.java
@@ -0,0 +1,69 @@
+package ar.edu.utn.frba.dds.domain.heladera;
+
+import ar.edu.utn.frba.dds.domain.Vianda;
+import ar.edu.utn.frba.dds.domain.colaborador.Colaborador;
+import ar.edu.utn.frba.dds.domain.ubicacion.Ubicacion;
+
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+public class Heladera {
+  private final float temperaturaMinimaCelsius;
+  private final float temperaturaMaximaCelsius;
+  private String nombre;
+  private Ubicacion ubicacion;
+  private int capacidadEnViandas;
+  private ZonedDateTime fechaInstalacion;
+  private List<Vianda> viandas;
+  private float temperaturaDeseadaCelsius;
+  private float ultimaTempRegistradaCelsius;
+  private ZonedDateTime momentoUltimaTempRegistrada;
+  private Colaborador colaboradorACargo;
+
+  public Heladera(float temperaturaMinimaCelsius, float temperaturaMaximaCelsius, float temperaturaDeseadaCelsius) {
+    this.temperaturaMinimaCelsius = temperaturaMinimaCelsius;
+    this.temperaturaMaximaCelsius = temperaturaMaximaCelsius;
+    this.temperaturaDeseadaCelsius = temperaturaDeseadaCelsius;
+  }
+
+  private boolean ultimaTemperaturaEsVieja() {
+    ZonedDateTime haceCincoMinutos = ZonedDateTime.now().minusMinutes(5);
+    return momentoUltimaTempRegistrada.isBefore(haceCincoMinutos);
+  }
+
+  private boolean temperaturaEstaDentroDeRangoDeModelo() {
+    return ultimaTempRegistradaCelsius > temperaturaMaximaCelsius || ultimaTempRegistradaCelsius < temperaturaMinimaCelsius;
+  }
+
+  private boolean temperaturaEstaDentroDeMargenDeTolerancia() {
+    return ultimaTempRegistradaCelsius < temperaturaDeseadaCelsius + 2 && ultimaTempRegistradaCelsius > temperaturaDeseadaCelsius - 2;
+  }
+
+  public EstadoDeFuncionamiento getEstado() {
+    if (ultimaTemperaturaEsVieja()) {
+      return EstadoDeFuncionamiento.EN_FALLA;
+    } else if (temperaturaEstaDentroDeRangoDeModelo()) {
+      return EstadoDeFuncionamiento.DEFICIENTE;
+    } else if (temperaturaEstaDentroDeMargenDeTolerancia()) {
+      return EstadoDeFuncionamiento.REPOSANDO;
+    } else {
+      return EstadoDeFuncionamiento.ENFRIANDO;
+    }
+  }
+
+  public Vianda sacarVianda() {
+    // TODO implement here
+    return null;
+  }
+
+  public void ingresarViandas(List<Vianda> viandas) {
+    // TODO implement here
+  }
+
+  public int mesesActiva() {
+    // TODO: testear
+    if (!isEnAlta()) return 0;  // TODO: Es lo mismo activa que en alta?
+    return (int) ChronoUnit.MONTHS.between(fechaInstalacion, ZonedDateTime.now());
+  }
+}
