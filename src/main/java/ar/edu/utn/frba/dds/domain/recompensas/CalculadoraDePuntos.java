39c2c8b04720b041d1d840dd39ed16f7a0366adf
nicolaslazo <45973144+nicolaslazo@users.noreply.github.com>
2024-05-26 18:39:30 -0300

    CalculadoraDePuntos actualizada para usar properties

--- src/main/java/ar/edu/utn/frba/dds/domain/recompensas/CalculadoraDePuntos.java
+++ src/main/java/ar/edu/utn/frba/dds/domain/recompensas/CalculadoraDePuntos.java
@@ -1,17 +1,24 @@
 package ar.edu.utn.frba.dds.domain.recompensas;
 
+import ar.edu.utn.frba.dds.config.ConfigLoader;
 import ar.edu.utn.frba.dds.domain.colaborador.Colaborador;
 
+// TODO: Hay alguna manera de hacerla estática? Nunca vamos a necesitar múltiples instancias de esta clase
 public class CalculadoraDePuntos {
-  // COEFICIENTES - Depende del archivo properties
-  // TODO: Cargar desde el properties
-  private double coeficientePesosDonados = 0.5;
-  private double coeficienteViandasDistribuidas = 1;
-  private double coeficienteViandasDonadas = 1.5;
-  private double coeficienteTarjetasRepartidas = 2;
-  private double coeficienteHeladerasActivas = 5;
+  private static final ConfigLoader coeficientesConfig =
+      new ConfigLoader("application.properties");
+  private static final double coeficientePesosDonados =
+      Double.parseDouble(coeficientesConfig.getProperty("puntaje.coeficiente.pesosDonados"));
+  private static final double coeficienteViandasDistribuidas =
+      Double.parseDouble(coeficientesConfig.getProperty("puntaje.coeficiente.viandasDistribuidas"));
+  private static final double coeficienteViandasDonadas =
+      Double.parseDouble(coeficientesConfig.getProperty("puntaje.coeficiente.viandasDonadas"));
+  private static final double coeficienteTarjetasRepartidas =
+      Double.parseDouble(coeficientesConfig.getProperty("puntaje.coeficiente.tarjetasRepartidas"));
+  private static final double coeficienteHeladerasActivas =
+      Double.parseDouble(coeficientesConfig.getProperty("puntaje.coeficiente.heladerasActivas"));
 
-  public double calcularPuntos(Colaborador colaborador) {
+  public static double calcularPuntos(Colaborador colaborador) {
     return (
         colaborador.getDineroDonado() * coeficientePesosDonados +
             colaborador.getNumeroViandasDistribuidas() * coeficienteViandasDistribuidas +
