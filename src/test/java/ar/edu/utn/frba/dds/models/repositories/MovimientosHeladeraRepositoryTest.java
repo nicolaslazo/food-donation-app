78fa29d6d5363632101d2dd5276b2877742be7ff
nicolaslazo <45973144+nicolaslazo@users.noreply.github.com>
2024-05-27 13:43:54 -0300

    Repositorio de movimientos de heladera agregado

---
+++ src/test/java/ar/edu/utn/frba/dds/models/repositories/MovimientosHeladeraRepositoryTest.java
@@ -0,0 +1,33 @@
+package ar.edu.utn.frba.dds.models.repositories;
+
+import ar.edu.utn.frba.dds.models.entities.heladera.EventoMovimiento;
+import ar.edu.utn.frba.dds.models.entities.heladera.Heladera;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.time.ZonedDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+
+class MovimientosHeladeraRepositoryTest {
+  MovimientosHeladeraRepository repositorio;
+
+  @BeforeEach
+  void setUp() {
+    repositorio = new MovimientosHeladeraRepository();
+  }
+
+  @Test
+  void repositorioSeInstancia() {
+    assertInstanceOf(MovimientosHeladeraRepository.class, repositorio);
+  }
+
+  @Test
+  void testInsert() {
+    EventoMovimiento evento = new EventoMovimiento(Mockito.mock(Heladera.class), ZonedDateTime.now());
+
+    assertEquals(1, repositorio.insert(evento));
+  }
+}
