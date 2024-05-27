785a27ce887f6dac2598e434b03aae56662d0b9b
Fernando Elian Benitez <febenitez@frba.utn.edu.ar>
2024-05-27 12:14:15 -0300

    TecnicoRepository creado

--- src/main/java/ar/edu/utn/frba/dds/models/entities/Tecnico.java
+++ src/main/java/ar/edu/utn/frba/dds/models/entities/Tecnico.java
@@ -3,9 +3,13 @@ package ar.edu.utn.frba.dds.models.entities;
 import ar.edu.utn.frba.dds.models.entities.contacto.Contacto;
 import ar.edu.utn.frba.dds.models.entities.documentacion.Documento;
 import ar.edu.utn.frba.dds.models.entities.ubicacion.AreaGeografica;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
+@Getter
+@Setter
 public class Tecnico {
   private String nombre;
   private String apellido;
---
+++ src/main/java/ar/edu/utn/frba/dds/models/repositories/TecnicoRepository.java
@@ -0,0 +1,41 @@
+package ar.edu.utn.frba.dds.models.repositories;
+
+import ar.edu.utn.frba.dds.models.entities.Tecnico;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class TecnicoRepository {
+    private final List<Tecnico> tecnicos;
+
+    public TecnicoRepository() { tecnicos = new ArrayList<Tecnico>(); }
+
+    public Optional<Tecnico> getTecnico(String cuil) {
+        return tecnicos
+                .stream()
+                .filter(tecnico -> tecnico.getCuil().equals(cuil))
+                .findFirst();
+    }
+
+    public List<Tecnico> getTecnicos() {
+        return tecnicos;
+    }
+
+    public void insertTecnico(Tecnico tecnico) {
+        tecnicos.add(tecnico);
+    }
+
+    public boolean deleteTecnico(String cuil) {
+        Optional<Tecnico> tecnicoOptional = getTecnico(cuil);
+        return tecnicoOptional.map(tecnicos::remove).orElse(false);
+    }
+
+    public boolean updateTecnico(Tecnico tecnico) {
+        //TODO
+        return true;
+    }
+
+
+
+}
