05a2996598e8770fd62cdd786f4eb075a32fe6f4
nicolaslazo <45973144+nicolaslazo@users.noreply.github.com>
2024-05-27 00:32:44 -0300

    OfertaRecompensa simplificada

--- src/main/java/ar/edu/utn/frba/dds/domain/contribucion/OfertaRecompensa.java
+++ src/main/java/ar/edu/utn/frba/dds/domain/contribucion/OfertaRecompensa.java
@@ -2,7 +2,6 @@ package ar.edu.utn.frba.dds.domain.contribucion;
 
 import ar.edu.utn.frba.dds.domain.colaborador.Colaborador;
 import ar.edu.utn.frba.dds.domain.recompensas.Recompensa;
-import lombok.Getter;
 import lombok.NonNull;
 
 import java.time.ZonedDateTime;
@@ -13,26 +12,13 @@ public class OfertaRecompensa extends Contribucion {
   @NonNull
   private final ZonedDateTime fecha;
   @NonNull
-  private final String nombre;
-  @NonNull
-  private final RubroRecompensa rubro;
-  private final double costoEnPuntos;
-
-  private int cantidadOfertas;
-
-  private URL imagen;
+  private final Recompensa recompensa;
 
   public OfertaRecompensa(@NonNull Colaborador colaborador, @NonNull ZonedDateTime fecha, @NonNull Recompensa recompensa) {
     super(colaborador, fecha);
     this.colaborador = colaborador;
     this.fecha = fecha;
-    this.nombre = nombre;
-    this.rubro = rubro;
-    this.costoEnPuntos = costoEnPuntos;
-    this.imagen = imagen;
+    this.recompensa = recompensa;
   }
-
-  public void usarOferta()
-  {this.cantidadDeOfertas=-1;}
 }
 
