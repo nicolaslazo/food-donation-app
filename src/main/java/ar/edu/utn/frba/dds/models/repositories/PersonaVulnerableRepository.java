dad210bc01fe1ff85fac1642c39a780171a3c33d
Fernando Elian Benitez <febenitez@frba.utn.edu.ar>
2024-05-27 18:27:25 -0300

    Se instancia la persona vulnerable y su respectiva tarjeta

--- src/main/java/ar/edu/utn/frba/dds/models/repositories/PersonaVulnerableRepository.java
+++ src/main/java/ar/edu/utn/frba/dds/models/repositories/PersonaVulnerableRepository.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class PersonaVulnerableRepository {
+public class PersonaVulnerableRepository implements IPersonaVulnerableRepository {
 
   private final List<PersonaVulnerable> personasVulnerables;
 
