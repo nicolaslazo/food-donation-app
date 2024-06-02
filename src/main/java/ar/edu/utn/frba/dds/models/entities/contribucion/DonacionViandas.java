05fa8493ba668a66586acaace0393ef94512bcfe
nicolaslazo <45973144+nicolaslazo@users.noreply.github.com>
2024-06-02 20:18:28 -0300

    Repositorio para donaciones de viandas agregado

--- src/main/java/ar/edu/utn/frba/dds/models/entities/contribucion/DonacionViandas.java
+++ src/main/java/ar/edu/utn/frba/dds/models/entities/contribucion/DonacionViandas.java
@@ -4,6 +4,7 @@ import ar.edu.utn.frba.dds.models.entities.Vianda;
 import ar.edu.utn.frba.dds.models.entities.colaborador.Colaborador;
 import lombok.Getter;
 import lombok.NonNull;
+import lombok.Setter;
 
 import java.time.ZonedDateTime;
 import java.util.List;
@@ -16,6 +17,9 @@ public class DonacionViandas extends Contribucion {
   @NonNull
   @Getter
   private final List<Vianda> viandas;
+  @Getter
+  @Setter
+  private int id;
 
   public DonacionViandas(@NonNull Colaborador colaborador, @NonNull ZonedDateTime fecha, @NonNull List<Vianda> viandas) {
     super(colaborador, fecha);
