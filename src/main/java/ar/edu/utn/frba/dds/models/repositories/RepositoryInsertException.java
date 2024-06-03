9a12ba05d423e04c74b9ad27d1d01887a9f3b080
nicolaslazo <45973144+nicolaslazo@users.noreply.github.com>
2024-06-02 21:07:09 -0300

    Excepción para inserts falladas agregada

---
+++ src/main/java/ar/edu/utn/frba/dds/models/repositories/RepositoryInsertException.java
@@ -0,0 +1,7 @@
+package ar.edu.utn.frba.dds.models.repositories;
+
+public class RepositoryInsertException extends Exception {
+  public RepositoryInsertException(String message) {
+    super(message);
+  }
+}
