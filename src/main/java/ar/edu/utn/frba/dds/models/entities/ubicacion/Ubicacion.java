61d5c091bb103de89a27d47e192b2a68335903c7
nicolaslazo <45973144+nicolaslazo@users.noreply.github.com>
2024-06-02 21:20:47 -0300

    Linting

--- src/main/java/ar/edu/utn/frba/dds/models/entities/ubicacion/Ubicacion.java
+++ src/main/java/ar/edu/utn/frba/dds/models/entities/ubicacion/Ubicacion.java
@@ -1,5 +1,7 @@
 package ar.edu.utn.frba.dds.models.entities.ubicacion;
 
+import java.util.Objects;
+
 public record Ubicacion(
     Coordenadas coordenadas, String casa, String categoria, String cercaDe, String numeroDeCasa, String calle,
     String unidad, String piso,
@@ -16,5 +18,18 @@ public record Ubicacion(
   public static Ubicacion desdeDireccion(String direccion) {
     // TODO: implement here
     return null;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Ubicacion ubicacion = (Ubicacion) o;
+    return Objects.equals(coordenadas, ubicacion.coordenadas);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(coordenadas);
   }
 }
--- src/main/java/ar/edu/utn/frba/dds/models/repositories/IPersonaVulnerableRepository.java
+++ src/main/java/ar/edu/utn/frba/dds/models/repositories/IPersonaVulnerableRepository.java
@@ -1,10 +1,12 @@
 package ar.edu.utn.frba.dds.models.repositories;
 
+import ar.edu.utn.frba.dds.models.entities.documentacion.Documento;
 import ar.edu.utn.frba.dds.models.entities.personaVulnerable.PersonaVulnerable;
 
 import java.util.Optional;
 
 public interface IPersonaVulnerableRepository {
-    public void insert(PersonaVulnerable personaVulnerable);
-    public Optional<PersonaVulnerable> get(int id);
+  public void insert(PersonaVulnerable personaVulnerable) throws RepositoryInsertException;
+
+  public Optional<PersonaVulnerable> get(Documento documento);
 }
