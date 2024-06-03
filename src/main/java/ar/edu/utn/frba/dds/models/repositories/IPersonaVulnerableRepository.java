cfb9b89990a601651a5649c587f868f99256bc92
nicolaslazo <45973144+nicolaslazo@users.noreply.github.com>
2024-06-02 21:36:33 -0300

    Más linting

--- src/main/java/ar/edu/utn/frba/dds/models/repositories/IPersonaVulnerableRepository.java
+++ src/main/java/ar/edu/utn/frba/dds/models/repositories/IPersonaVulnerableRepository.java
@@ -6,7 +6,7 @@ import ar.edu.utn.frba.dds.models.entities.personaVulnerable.PersonaVulnerable;
 import java.util.Optional;
 
 public interface IPersonaVulnerableRepository {
-  public void insert(PersonaVulnerable personaVulnerable) throws RepositoryInsertException;
+  void insert(PersonaVulnerable personaVulnerable) throws RepositoryInsertException;
 
-  public Optional<PersonaVulnerable> get(Documento documento);
+  Optional<PersonaVulnerable> get(Documento documento);
 }
