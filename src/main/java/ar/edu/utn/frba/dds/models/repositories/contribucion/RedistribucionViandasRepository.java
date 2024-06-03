5e09d65604186bba0dc747c88b6cf074c07296bd
nicolaslazo <45973144+nicolaslazo@users.noreply.github.com>
2024-06-02 21:06:05 -0300

    Repositorio para redistribuciones de viandas agregado

---
+++ src/main/java/ar/edu/utn/frba/dds/models/repositories/contribucion/RedistribucionViandasRepository.java
@@ -0,0 +1,48 @@
+package ar.edu.utn.frba.dds.models.repositories.contribucion;
+
+import ar.edu.utn.frba.dds.models.entities.colaborador.Colaborador;
+import ar.edu.utn.frba.dds.models.entities.contribucion.RedistribucionViandas;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class RedistribucionViandasRepository {
+  static RedistribucionViandasRepository instancia = null;
+  final List<RedistribucionViandas> redistribuciones;
+
+  public RedistribucionViandasRepository() {
+    this.redistribuciones = new ArrayList<>();
+  }
+
+  public static RedistribucionViandasRepository getInstancia() {
+    if (instancia == null) {
+      instancia = new RedistribucionViandasRepository();
+    }
+
+    return instancia;
+  }
+
+  public Optional<RedistribucionViandas> get(int id) {
+    return redistribuciones.stream().filter(redistribucion -> redistribucion.getId() == id).findFirst();
+  }
+
+  public int getTotal(Colaborador colaborador) {
+    return redistribuciones
+        .stream()
+        .filter(redistribucion -> redistribucion.getColaborador() == colaborador)
+        .mapToInt(RedistribucionViandas::getNumeroViandas)
+        .sum();
+  }
+
+  public int insert(RedistribucionViandas redistribucion) {
+    redistribuciones.add(redistribucion);
+    redistribucion.setId(redistribuciones.size());
+
+    return redistribucion.getId();
+  }
+
+  public void deleteTodas() {
+    redistribuciones.clear();
+  }
+}
