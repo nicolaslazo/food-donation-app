320889ad2ac4580faabcdf2cd4bc3eb81a71207f
nicolaslazo <45973144+nicolaslazo@users.noreply.github.com>
2024-06-21 13:15:33 -0300

    Repository de suscripciones actualizado para la estructura nueva

--- src/main/java/ar/edu/utn/frba/dds/models/repositories/contacto/SuscripcionRepository.java
+++ src/main/java/ar/edu/utn/frba/dds/models/repositories/contacto/SuscripcionRepository.java
@@ -1,8 +1,9 @@
-package ar.edu.utn.frba.dds.models.repositories.heladera;
+package ar.edu.utn.frba.dds.models.repositories.contacto;
 
 import ar.edu.utn.frba.dds.models.entities.colaborador.Colaborador;
+import ar.edu.utn.frba.dds.models.entities.contacto.Suscripcion;
+import ar.edu.utn.frba.dds.models.entities.contacto.TipoNotificacion;
 import ar.edu.utn.frba.dds.models.entities.heladera.Heladera;
-import ar.edu.utn.frba.dds.models.entities.heladera.Suscripcion;
 import ar.edu.utn.frba.dds.models.repositories.RepositoryInsertException;
 
 import java.util.ArrayList;
@@ -33,15 +34,33 @@ public class SuscripcionRepository {
     return suscripciones.stream().filter(suscripcion -> suscripcion.getHeladera() == heladera).toList();
   }
 
-  public Optional<Suscripcion> get(Heladera heladera, Colaborador colaborador) {
+  public List<Suscripcion> get(Heladera heladera, TipoNotificacion tipoNotificacion) {
+    return suscripciones
+        .stream()
+        .filter(suscripcion ->
+            suscripcion.getHeladera() == heladera && suscripcion.getTipoNotificacion() == tipoNotificacion)
+        .toList();
+  }
+
+  public List<Suscripcion> get(Heladera heladera, Colaborador colaborador) {
     return suscripciones
         .stream()
         .filter(suscripcion -> suscripcion.getHeladera() == heladera && suscripcion.getColaborador() == colaborador)
-        .findFirst();
+        .toList();
+  }
+
+  public Optional<Suscripcion> get(Heladera heladera, TipoNotificacion tipoNotificacion, Colaborador colaborador) {
+    return suscripciones
+        .stream()
+        .filter(suscripcion ->
+            suscripcion.getHeladera() == heladera &&
+                suscripcion.getTipoNotificacion() == tipoNotificacion &&
+                suscripcion.getColaborador() == colaborador
+        ).findFirst();
   }
 
   public int insert(Suscripcion suscripcion) throws RepositoryInsertException {
-    if (get(suscripcion.getHeladera(), suscripcion.getColaborador()).isPresent()) {
+    if (get(suscripcion.getHeladera(), suscripcion.getTipoNotificacion(), suscripcion.getColaborador()).isPresent()) {
       throw new RepositoryInsertException("Ese colaborador ya está inscrito a esa heladera");
     }
 
