e2ea524a34e4134d9431bb515986b4f9b13b73f3
Guadalupe Hryb <ghrybalvarez@frba.utn.edu.ar>
2024-06-23 12:29:01 -0300

    Agrego lo de regex que hizo nico

--- src/main/java/ar/edu/utn/frba/dds/models/entities/TarjetaColaborador.java
+++ src/main/java/ar/edu/utn/frba/dds/models/entities/TarjetaColaborador.java
@@ -1,18 +1,21 @@
 package ar.edu.utn.frba.dds.models.entities;
 
 import ar.edu.utn.frba.dds.models.entities.colaborador.Colaborador;
-import lombok.Getter;
+
 import lombok.NonNull;
+import java.util.regex.Pattern;
 
 import java.util.Objects;
 
 public class TarjetaColaborador {
-  @Getter
+  private static final String REGEX_IDENTIFICADOR_TARJETA =
+      "^[0-9a-fA-F]{8}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{12}$";
   private final @NonNull String identificador;
-  @Getter
   private final @NonNull Colaborador recipiente;
 
-  public TarjetaColaborador(@NonNull String identificador, @NonNull Colaborador recipiente) {
+  public TarjetaColaborador(@NonNull String identificador, @NonNull Colaborador recipiente) throws Exception
+  {if (!Pattern.matches(REGEX_IDENTIFICADOR_TARJETA, identificador)) {
+    throw new Exception("El identificador proveído no es válido");}
     this.identificador = identificador;
     this.recipiente = recipiente;
   }
