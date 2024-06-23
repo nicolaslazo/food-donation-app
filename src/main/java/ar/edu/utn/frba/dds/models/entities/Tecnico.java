5e3233fa156db4db0c773e0736b7ec5149775801
nicolaslazo <45973144+nicolaslazo@users.noreply.github.com>
2024-06-23 01:45:21 -0300

    Tecnico puede decir si una heladera existe en su rango de servicio

--- src/main/java/ar/edu/utn/frba/dds/models/entities/Tecnico.java
+++ src/main/java/ar/edu/utn/frba/dds/models/entities/Tecnico.java
@@ -2,7 +2,9 @@ package ar.edu.utn.frba.dds.models.entities;
 
 import ar.edu.utn.frba.dds.models.entities.contacto.Contacto;
 import ar.edu.utn.frba.dds.models.entities.documentacion.Documento;
+import ar.edu.utn.frba.dds.models.entities.heladera.Heladera;
 import ar.edu.utn.frba.dds.models.entities.ubicacion.AreaGeografica;
+import ar.edu.utn.frba.dds.models.entities.ubicacion.CalculadoraDistancia;
 import lombok.Getter;
 
 import java.util.List;
@@ -20,5 +22,10 @@ public class Tecnico {
 
   public Tecnico(String cuil, AreaGeografica areaAsignada) {
     this.cuil = cuil;
+    this.areaAsignada = areaAsignada;
+  }
+
+  public boolean isDentroDeRango(Heladera heladera) {
+    return CalculadoraDistancia.calcular(heladera.getUbicacion(), areaAsignada.centro()) < areaAsignada.radioEnMetros();
   }
 }
