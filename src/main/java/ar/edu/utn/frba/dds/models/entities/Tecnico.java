0a381a93dd4176ff49c53a04ee836eb0a931276f
nicolaslazo <45973144+nicolaslazo@users.noreply.github.com>
2024-06-26 23:12:10 -0300

    Clase Tecnico tiene un Usuario, constructor más detallado

--- src/main/java/ar/edu/utn/frba/dds/models/entities/Tecnico.java
+++ src/main/java/ar/edu/utn/frba/dds/models/entities/Tecnico.java
@@ -5,24 +5,35 @@ import ar.edu.utn.frba.dds.models.entities.documentacion.Documento;
 import ar.edu.utn.frba.dds.models.entities.heladera.Heladera;
 import ar.edu.utn.frba.dds.models.entities.ubicacion.AreaGeografica;
 import ar.edu.utn.frba.dds.models.entities.ubicacion.CalculadoraDistancia;
+import ar.edu.utn.frba.dds.models.entities.users.Usuario;
 import lombok.Getter;
+import lombok.NonNull;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
-public class Tecnico {
-  private String nombre;
-  private String apellido;
-  private Documento documento;
+public final class Tecnico {
+  private final @NonNull Documento documento;
   @Getter
-  private final String cuil;
-  private List<Contacto> contactos;
+  private final @NonNull String cuil;
+  private final @NonNull List<Contacto> contactos;
+  private final @NonNull Usuario usuario;
+  private @NonNull String nombre;
+  private @NonNull String apellido;
   @Getter
-  private AreaGeografica areaAsignada;
+  private @NonNull AreaGeografica areaAsignada;
 
-  public Tecnico(String cuil, AreaGeografica areaAsignada) {
+  public Tecnico(@NonNull String nombre, @NonNull String apellido, @NonNull Documento documento,
+                 @NonNull String cuil, List<Contacto> contactos, @NonNull AreaGeografica areaAsignada,
+                 @NonNull Usuario usuario) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.documento = documento;
     this.cuil = cuil;
+    this.contactos = contactos != null ? contactos : new ArrayList<>();
     this.areaAsignada = areaAsignada;
+    this.usuario = usuario;
   }
 
   public boolean isDentroDeRango(Heladera heladera) {
