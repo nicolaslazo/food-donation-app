031aa56b82fe56b9b1b16cde0e91b5f06b7dba79
nicolaslazo <45973144+nicolaslazo@users.noreply.github.com>
2024-06-26 23:08:43 -0300

    Overhaul de la clase usuario

--- src/main/java/ar/edu/utn/frba/dds/models/entities/users/Usuario.java
+++ src/main/java/ar/edu/utn/frba/dds/models/entities/users/Usuario.java
@@ -1,12 +1,22 @@
 package ar.edu.utn.frba.dds.models.entities.users;
 
+import ar.edu.utn.frba.dds.auth.GeneradorDeContrasenias;
+import ar.edu.utn.frba.dds.models.entities.contacto.Email;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NonNull;
 
-@Getter
-@Setter
 public class Usuario {
-  private String nombreDeUsuario;
-  private String contrasenia;
-  private Rol rol;
+  private final @NonNull Email mail;
+  private @NonNull String contrasenia;
+  @Getter
+  private @NonNull Rol rol;
+
+  public Usuario(Email mail, Rol rol) {
+    this.mail = mail;
+    this.contrasenia = GeneradorDeContrasenias.generarContrasenia();
+    this.rol = rol;
+
+    // TODO: Terminar el setup de envío de mails y configurar que enviarMail sea un no-op durante tests
+    // new EnviadorDeMails().enviarMail(mail.destinatario(), this.contrasenia);
+  }
 }
