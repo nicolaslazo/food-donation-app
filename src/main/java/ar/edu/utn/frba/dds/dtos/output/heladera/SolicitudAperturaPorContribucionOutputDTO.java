f444e693fcfc6f402dd9ed20d056e393fcad8b43
nicolaslazo <45973144+nicolaslazo@users.noreply.github.com>
2024-06-29 13:23:28 -0300

    DTO de solicitudes de apertura implementado

---
+++ src/main/java/ar/edu/utn/frba/dds/dtos/output/heladera/SolicitudAperturaPorContribucionOutputDTO.java
@@ -0,0 +1,25 @@
+package ar.edu.utn.frba.dds.dtos.output.heladera;
+
+import ar.edu.utn.frba.dds.models.entities.heladera.SolicitudAperturaPorContribucion;
+import com.google.gson.Gson;
+import lombok.NonNull;
+
+import java.util.UUID;
+
+public record SolicitudAperturaPorContribucionOutputDTO(
+    @NonNull UUID identificadorTarjeta,
+    int idSolicitud,
+    @NonNull String fechaVencimientoSerializadaIso8601,
+    @NonNull double[] pesosViandasEnGramos
+) {
+  public SolicitudAperturaPorContribucionOutputDTO(SolicitudAperturaPorContribucion solicitud) {
+    this(solicitud.getTarjeta().getId(),
+        solicitud.getId(),
+        solicitud.getFechaVencimiento().toString(),
+        solicitud.getPesosDeViandasEnGramos());
+  }
+
+  public String aJson() {
+    return new Gson().toJson(this);
+  }
+}
