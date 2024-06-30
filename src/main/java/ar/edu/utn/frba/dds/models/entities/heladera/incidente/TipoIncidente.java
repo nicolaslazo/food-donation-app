0797411cfbe3099abae142a642cd2c8cd49c7ac4
nicolaslazo <45973144+nicolaslazo@users.noreply.github.com>
2024-06-30 00:23:15 -0300

    Entidad de incidente implementada

---
+++ src/main/java/ar/edu/utn/frba/dds/models/entities/heladera/incidente/TipoIncidente.java
@@ -0,0 +1,9 @@
+package ar.edu.utn.frba.dds.models.entities.heladera.incidente;
+
+public enum TipoIncidente {
+  FALLA_REPORTADA_POR_COLABORADOR,
+  BAJA_TEMPERATURA,
+  ALTA_TEMPERATURA,
+  FALLA_CONEXION,
+  FRAUDE
+}
