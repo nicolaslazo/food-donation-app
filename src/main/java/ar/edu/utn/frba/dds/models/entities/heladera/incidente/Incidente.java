09b807e98336912e2ae7bc5a3b850aec672b47db
nicolaslazo <45973144+nicolaslazo@users.noreply.github.com>
2024-06-30 00:42:37 -0300

    Finals apropiados agregados

--- src/main/java/ar/edu/utn/frba/dds/models/entities/heladera/incidente/Incidente.java
+++ src/main/java/ar/edu/utn/frba/dds/models/entities/heladera/incidente/Incidente.java
@@ -2,7 +2,6 @@ package ar.edu.utn.frba.dds.models.entities.heladera.incidente;
 
 import ar.edu.utn.frba.dds.models.entities.colaborador.Colaborador;
 import ar.edu.utn.frba.dds.models.entities.heladera.Heladera;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.Setter;
@@ -10,21 +9,27 @@ import lombok.Setter;
 import java.net.URL;
 import java.time.ZonedDateTime;
 
-@AllArgsConstructor
 public class Incidente {
+  final @NonNull Heladera heladera;
+  final @NonNull TipoIncidente tipo;
+  final @NonNull ZonedDateTime fecha;
+  final Colaborador colaborador;
+  final String descripcion;
+  final URL imagen;
   @Getter
   @Setter
   int idIncidente;
-  @NonNull Heladera heladera;
-  @NonNull TipoIncidente tipo;
-  @NonNull ZonedDateTime fecha;
-  Colaborador colaborador;
-  String descripcion;
-  URL imagen;
 
-  public Incidente(@NonNull Heladera heladera, @NonNull TipoIncidente tipo, @NonNull ZonedDateTime fecha) {
+  public Incidente(@NonNull Heladera heladera, @NonNull TipoIncidente tipo, @NonNull ZonedDateTime fecha, Colaborador colaborador, String descripcion, URL imagen) {
     this.heladera = heladera;
     this.tipo = tipo;
     this.fecha = fecha;
+    this.colaborador = colaborador;
+    this.descripcion = descripcion;
+    this.imagen = imagen;
+  }
+
+  public Incidente(@NonNull Heladera heladera, @NonNull TipoIncidente tipo, @NonNull ZonedDateTime fecha) {
+    this(heladera, tipo, fecha, null, null, null);
   }
 }
