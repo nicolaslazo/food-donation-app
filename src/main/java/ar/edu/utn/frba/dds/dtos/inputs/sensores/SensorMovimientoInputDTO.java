99a7814ff375be5d9cd9b83b811a26b78e2a5d48
Fernando Elian Benitez <febenitez@frba.utn.edu.ar>
2024-07-01 23:46:37 -0300

    Timestamp agregado en los dtos de sensores

--- src/main/java/ar/edu/utn/frba/dds/dtos/inputs/sensores/SensorMovimientoInputDTO.java
+++ src/main/java/ar/edu/utn/frba/dds/dtos/inputs/sensores/SensorMovimientoInputDTO.java
@@ -3,13 +3,14 @@ package ar.edu.utn.frba.dds.dtos.inputs.sensores;
 import com.google.gson.Gson;
 import lombok.NonNull;
 
-public record SensorMovimientoInputDTO(@NonNull String msg) {
+import java.time.ZonedDateTime;
+
+public record SensorMovimientoInputDTO(int idHeladera,@NonNull String timestampIso8601) {
     public static SensorMovimientoInputDTO desdeJson(@NonNull String json) {
         return new Gson().fromJson(json, SensorMovimientoInputDTO.class);
     }
 
-    //TODO ¿Que recibiriamos?
-    public String msg() {
-        return msg;
+    public ZonedDateTime getTiempo() {
+        return ZonedDateTime.parse(timestampIso8601);
     }
 }
--- src/main/java/ar/edu/utn/frba/dds/dtos/inputs/sensores/SensorTemperaturaInputDTO.java
+++ src/main/java/ar/edu/utn/frba/dds/dtos/inputs/sensores/SensorTemperaturaInputDTO.java
@@ -3,13 +3,20 @@ package ar.edu.utn.frba.dds.dtos.inputs.sensores;
 import com.google.gson.Gson;
 import lombok.NonNull;
 
-public record SensorTemperaturaInputDTO(@NonNull String temperatura) {
+import java.time.ZonedDateTime;
+
+public record SensorTemperaturaInputDTO(int idHeladera,@NonNull String temperatura,
+                                        @NonNull String timestampIso8601) {
     public static SensorTemperaturaInputDTO desdeJson(@NonNull String json) {
         return new Gson().fromJson(json, SensorTemperaturaInputDTO.class);
     }
 
     public double getTemperatura() {
         return (double) Double.parseDouble(temperatura);
+    }
+
+    public ZonedDateTime getTiempo() {
+        return ZonedDateTime.parse(timestampIso8601);
     }
 
 }
