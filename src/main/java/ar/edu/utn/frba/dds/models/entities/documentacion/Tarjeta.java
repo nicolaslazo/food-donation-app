b3a5ce7762dd5856f0e523c08a827881b5ebb8af
nicolaslazo <45973144+nicolaslazo@users.noreply.github.com>
2024-08-07 22:55:08 -0300

    Fechas de alta y baja agregadas a Tarjeta

--- src/main/java/ar/edu/utn/frba/dds/models/entities/documentacion/Tarjeta.java
+++ src/main/java/ar/edu/utn/frba/dds/models/entities/documentacion/Tarjeta.java
@@ -1,33 +1,53 @@
 package ar.edu.utn.frba.dds.models.entities.documentacion;
 
 import ar.edu.utn.frba.dds.models.entities.colaborador.Colaborador;
-import ar.edu.utn.frba.dds.models.entities.users.Permiso;
+import ar.edu.utn.frba.dds.models.entities.users.PermisoDenegadoException;
 import ar.edu.utn.frba.dds.models.entities.users.Usuario;
 import lombok.Getter;
 import lombok.NonNull;
 
+import java.time.ZonedDateTime;
 import java.util.Objects;
 import java.util.UUID;
 
+@Getter
 public class Tarjeta {
-  @Getter
   final @NonNull UUID id;
-  final @NonNull Colaborador proveedor;
-  @Getter
-  final @NonNull Usuario recipiente;
+  Colaborador proveedor = null;
+  Usuario recipiente = null;
+  ZonedDateTime fechaAlta = null;
+  ZonedDateTime fechaBaja = null;
+  Usuario responsableDeBaja = null;
 
-  public Tarjeta(@NonNull UUID id, @NonNull Colaborador proveedor, @NonNull Usuario recipiente) {
+  public Tarjeta(@NonNull UUID id) {
     this.id = id;
-    this.proveedor = proveedor;
-    this.recipiente = recipiente;
   }
 
-  public void assertTienePermiso(@NonNull Permiso permiso, @NonNull String razon) {
-    recipiente.assertTienePermiso(permiso, razon);
-  }
+  public void assertTienePermiso(@NonNull String nombrePermiso, @NonNull String razon) throws PermisoDenegadoException {
+    assertEstaVigente();
 
-  public void assertTienePermiso(@NonNull String nombrePermiso, @NonNull String razon) {
     recipiente.assertTienePermiso(nombrePermiso, razon);
+  }
+
+  void assertEstaVigente() throws PermisoDenegadoException {
+    if (fechaBaja != null) throw new PermisoDenegadoException("Esta tarjeta ya fue dada de baja");
+    if (fechaAlta == null) throw new PermisoDenegadoException("Esta tarjeta no fue dada de alta");
+  }
+
+  public void setEnAlta(@NonNull Usuario recipiente, @NonNull Colaborador proveedor, @NonNull ZonedDateTime timestamp)
+      throws PermisoDenegadoException {
+    if (this.fechaAlta != null) throw new PermisoDenegadoException("Esta tarjeta ya está en alta");
+
+    this.recipiente = recipiente;
+    this.proveedor = proveedor;
+    this.fechaAlta = timestamp;
+  }
+
+  public void setDeBaja(@NonNull Usuario quien, @NonNull ZonedDateTime timestamp) throws PermisoDenegadoException {
+    assertEstaVigente();
+
+    responsableDeBaja = quien;
+    fechaBaja = timestamp;
   }
 
   @Override
