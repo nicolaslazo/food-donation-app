0fa05bc89917914387cb52f5da52154ae7e0c67c
Guadalupe Hryb <ghrybalvarez@frba.utn.edu.ar>
2024-08-15 22:48:56 -0300

    Rediseño

--- src/web/canjePuntosConsumidor/script.js
+++ src/web/canjePuntosConsumidor/script.js
@@ -17,3 +17,44 @@ function filtrarPorCategoria() {
     });
 }
 
+window.onload = function() {
+    mostrarModal();
+};
+
+function canjearServicio(serviceId) {
+    console.log("Mostrando el modal y aplicando blur");
+
+    // Muestra el modal
+    document.getElementById('modal').style.display = 'block';
+
+    // Agrega la clase blur al contenedor de puntos
+    const pointsContainer = document.querySelector('.points-container');
+    pointsContainer.classList.add('blur-background');
+
+    // Desactiva la interacción con el fondo
+    pointsContainer.style.pointerEvents = 'none';
+}
+
+function cerrarModal() {
+    console.log("Cerrando el modal y quitando blur");
+
+    // Oculta el modal
+    document.getElementById('modal').style.display = 'none';
+
+    // Elimina la clase blur del contenedor de puntos
+    const pointsContainer = document.querySelector('.points-container');
+    pointsContainer.classList.remove('blur-background');
+
+    // Habilita la interacción con el fondo
+    pointsContainer.style.pointerEvents = 'auto';
+}
+
+function confirmarCanje() {
+    alert('¡Canje realizado con éxito!');
+    cerrarModal(); // Cierra el modal y elimina el desenfoque después del canje
+}
+
+
+
+
+
