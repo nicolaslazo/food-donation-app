fc6ee429adf00fe293b9d6eb3e04ff618948e64e
nicolaslazo <45973144+nicolaslazo@users.noreply.github.com>
2024-08-19 00:57:44 -0300

    Atributos compartidos delegados a Usuario

--- src/main/java/ar/edu/utn/frba/dds/models/entities/Tecnico.java
+++ src/main/java/ar/edu/utn/frba/dds/models/entities/Tecnico.java
@@ -12,6 +12,7 @@ import ar.edu.utn.frba.dds.models.entities.users.Usuario;
 import lombok.Getter;
 import lombok.NonNull;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -20,25 +21,29 @@ import java.util.List;
 public final class Tecnico {
   // TODO: el rol capaz debería estar almacenado en un repositorio?
   private static final Rol ROL_DEFAULT = new Rol("tecnico", new HashSet<>());
-  final @NonNull Documento documento;
   @Getter
   final @NonNull String cuil;
   final @NonNull List<Contacto> contactos;
   final @NonNull Usuario usuario;
-  @NonNull String nombre;
-  @NonNull String apellido;
   @Getter
   @NonNull AreaGeografica areaAsignada;
 
-  public Tecnico(@NonNull String nombre, @NonNull String apellido, @NonNull Documento documento,
-                 @NonNull String cuil, @NonNull Email email, @NonNull AreaGeografica areaAsignada) {
-    this.nombre = nombre;
-    this.apellido = apellido;
-    this.documento = documento;
+  public Tecnico(@NonNull Email email,
+                 @NonNull Documento documento,
+                 @NonNull String primerNombre,
+                 @NonNull String apellido,
+                 @NonNull LocalDate fechaNacimiento,
+                 @NonNull String cuil,
+                 @NonNull AreaGeografica areaAsignada) {
+    this.usuario = new Usuario(email,
+        documento,
+        primerNombre,
+        apellido,
+        fechaNacimiento,
+        new HashSet<>(List.of(ROL_DEFAULT)));  // TODO: Agregar permisos
     this.cuil = cuil;
     this.areaAsignada = areaAsignada;
     this.contactos = new ArrayList<>(List.of(email));
-    this.usuario = new Usuario(email, new HashSet<>(List.of(ROL_DEFAULT)));  // TODO: Agregar permisos
   }
 
   public void enviarMensaje(String mensaje) throws MensajeAContactoException {
