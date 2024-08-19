65f315b8b984ff6b0c34b0ae3e5dcb01b89b780c
Guadalupe Hryb <ghrybalvarez@frba.utn.edu.ar>
2024-08-19 00:44:58 -0300

    Correcciones 1

--- src/web/cargaPersonaVulnerable/script.js
+++ src/web/cargaPersonaVulnerable/script.js
@@ -1,27 +1,38 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const situationSelect = document.getElementById('situacion');
-    const addressSection = document.getElementById('direccion');
 
-    const hasDniSelect = document.getElementById('tiene-dni');
+    const dniSiRadio = document.getElementById('tiene-dni-si');
+    const dniNoRadio = document.getElementById('tiene-dni-no');
     const dniSection = document.getElementById('posee-documento');
 
-// Manejar la visibilidad del campo de dirección
-    situationSelect.addEventListener('change', function() {
-        if (this.value === 'domicilio') {
+    const domicilioSiRadio = document.getElementById('tiene-domicilio-si');
+    const domicilioNoRadio = document.getElementById('tiene-domicilio-no');
+    const addressSection = document.getElementById('direccion'); // El div que contiene los campos de la dirección
+
+    // Manejar la visibilidad de los campos de Domicilio cuando se selecciona una opción
+    domicilioSiRadio.addEventListener('change', function() {
+        if (this.checked) {
             addressSection.classList.add('show'); // Muestra el div
             addressSection.classList.remove('hidden'); // Asegura que la clase 'hidden' no esté presente
-        } else {
+        }
+    });
+
+    domicilioNoRadio.addEventListener('change', function() {
+        if (this.checked) {
             addressSection.classList.add('hidden'); // Oculta el div
             addressSection.classList.remove('show'); // Asegura que la clase 'show' no esté presente
         }
     });
 
-    // Manejar la visibilidad de los campos de DNI
-    hasDniSelect.addEventListener('change', function() {
-        if (this.value === 'yes') {
+ // Manejar la visibilidad de los campos de DNI cuando se selecciona una opción
+    dniSiRadio.addEventListener('change', function() {
+        if (this.checked) {
             dniSection.classList.add('show'); // Muestra el div
             dniSection.classList.remove('hidden'); // Asegura que la clase 'hidden' no esté presente
-        } else {
+        }
+    });
+
+    dniNoRadio.addEventListener('change', function() {
+        if (this.checked) {
             dniSection.classList.add('hidden'); // Oculta el div
             dniSection.classList.remove('show'); // Asegura que la clase 'show' no esté presente
         }
