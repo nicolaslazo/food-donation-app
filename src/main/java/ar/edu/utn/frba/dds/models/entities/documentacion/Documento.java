32c8573a7cc5b36f442f7b989afaac114b595d37
nicolaslazo <45973144+nicolaslazo@users.noreply.github.com>
2024-08-21 18:40:24 -0300

    Documento e Email hechas embeddeables

--- src/main/java/ar/edu/utn/frba/dds/models/entities/documentacion/Documento.java
+++ src/main/java/ar/edu/utn/frba/dds/models/entities/documentacion/Documento.java
@@ -2,5 +2,8 @@ package ar.edu.utn.frba.dds.models.entities.documentacion;
 
 import lombok.NonNull;
 
-public record Documento(@NonNull TipoDocumento tipo, int valor) {
+import javax.persistence.Embeddable;
+
+@Embeddable
+public record Documento(@NonNull TipoDocumento tipo, @NonNull Integer valor) {
 }
