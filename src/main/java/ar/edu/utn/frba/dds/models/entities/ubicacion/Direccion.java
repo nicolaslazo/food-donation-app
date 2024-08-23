8d342a55c3f4a5d013aa948f7ad204a46d83a4e6
nicolaslazo <45973144+nicolaslazo@users.noreply.github.com>
2024-08-22 23:53:20 -0300

    Ubicacion reemplazada por Direccion

---
+++ src/main/java/ar/edu/utn/frba/dds/models/entities/ubicacion/Direccion.java
@@ -0,0 +1,16 @@
+package ar.edu.utn.frba.dds.models.entities.ubicacion;
+
+import ar.edu.utn.frba.dds.models.entities.users.Usuario;
+import lombok.NonNull;
+
+public record Direccion(
+    @NonNull Usuario usuario,
+    String unidad,
+    String piso,
+    @NonNull String numeroDeCasa,
+    @NonNull String calle,
+    @NonNull String codigoPostal,
+    @NonNull String ciudad,
+    @NonNull String provincia,
+    @NonNull String pais) {
+}
