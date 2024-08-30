3c3939dc7851d79fdf5f0a45cb36b72ef1961f51
febenitez04 <febenitez@frba.utn.edu.ar>
2024-08-30 13:11:40 -0300

    HeladerasRepository extends HibernateEntityManager

--- src/main/java/ar/edu/utn/frba/dds/models/repositories/heladera/HeladerasRepository.java
+++ src/main/java/ar/edu/utn/frba/dds/models/repositories/heladera/HeladerasRepository.java
@@ -3,56 +3,70 @@ package ar.edu.utn.frba.dds.models.repositories.heladera;
 import ar.edu.utn.frba.dds.models.entities.colaborador.Colaborador;
 import ar.edu.utn.frba.dds.models.entities.heladera.Heladera;
 import ar.edu.utn.frba.dds.models.entities.ubicacion.CoordenadasGeograficas;
+import ar.edu.utn.frba.dds.models.repositories.HibernateEntityManager;
 import ar.edu.utn.frba.dds.models.repositories.RepositoryException;
 import ar.edu.utn.frba.dds.models.repositories.ViandasRepository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
-public class HeladerasRepository {
-  static HeladerasRepository instancia = null;
-  final List<Heladera> heladeras;
-
-  private HeladerasRepository() {
-    heladeras = new ArrayList<>();
-  }
-
-  public static HeladerasRepository getInstancia() {
-    if (instancia == null) instancia = new HeladerasRepository();
-
-    return instancia;
-  }
-
-  public Optional<Heladera> get(int id) {
-    return heladeras.stream().filter(heladera -> heladera.getId() == id).findFirst();
-  }
+public class HeladerasRepository extends HibernateEntityManager<Heladera, Long> {
 
   public Optional<Heladera> get(CoordenadasGeograficas ubicacion) {
-    return heladeras.stream().filter(heladera -> heladera.getUbicacion() == ubicacion).findFirst();
+    EntityManager em = entityManager();
+    CriteriaBuilder cb = em.getCriteriaBuilder();
+    CriteriaQuery<Heladera> query = cb.createQuery(Heladera.class);
+    Root<Heladera> root = query.from(Heladera.class);
+
+    query.select(root).where(cb.equal(root.get("ubicacion"), ubicacion));
+
+    return em.createQuery(query).getResultList().stream().findFirst();
   }
 
-  public List<Heladera> getHeladerasConTemperaturaDesactualizada(int limiteEnMinutos) {
-    return heladeras
-        .stream()
-        .filter(heladera -> heladera
-            .getMomentoUltimaTempRegistrada()
-            .isBefore(ZonedDateTime.now().minusMinutes(limiteEnMinutos)))
-        .toList();
+  public Stream<Heladera> getHeladerasConTemperaturaDesactualizada(int limiteEnMinutos) {
+    EntityManager em = entityManager();
+    CriteriaBuilder cb = em.getCriteriaBuilder();
+    CriteriaQuery<Heladera> query = cb.createQuery(Heladera.class);
+    Root<Heladera> root = query.from(Heladera.class);
+
+    ZonedDateTime limite = ZonedDateTime.now().minusMinutes(limiteEnMinutos);
+
+    query.select(root).where(cb.lessThan(root.get("momentoUltimaTempRegistrada"), limite));
+
+    return em.createQuery(query).getResultStream();
   }
 
-  public List<Heladera> getTodas() {
-    return heladeras;
+  public Stream<Heladera> getTodas(Colaborador encargado) {
+    EntityManager em = entityManager();
+    CriteriaBuilder cb = em.getCriteriaBuilder();
+    CriteriaQuery<Heladera> query = cb.createQuery(Heladera.class);
+    Root<Heladera> root = query.from(Heladera.class);
+
+    query.select(root).where(cb.equal(root.get("colaborador"), encargado));
+
+    return em.createQuery(query).getResultStream();
   }
 
-  public List<Heladera> getTodas(Colaborador encargado) {
-    return heladeras.stream().filter(heladera -> heladera.getEncargado() == encargado).toList();
+  public void updateTiempoHeladera(Long id, Heladera nuevaHeladera) {
+    Optional<Heladera> heladera = findById(id);
+    if(heladera.isPresent()) {
+      heladera.get().setUltimaTempRegistradaCelsius(
+              nuevaHeladera.getUltimaTempRegistradaCelsius()
+      );
+      persist(heladera.get());
+    }
   }
 
   public int getMesesActivosCumulativos(Colaborador colaborador) {
     // TODO: Actualizar en base a la fecha del último incidente resuelto
-    return getTodas(colaborador).stream().mapToInt(Heladera::mesesActiva).sum();
+    return getTodas(colaborador).mapToInt(Heladera::mesesActiva).sum();
   }
 
   /* Este método concierne a la cantidad de viandas ahora mismo depositadas en la heladera, independientemente de las
@@ -64,31 +78,9 @@ public class HeladerasRepository {
 
   public int getCapacidadDisponible(Heladera heladera) {
     final int viandasEnContribucionesVigentes =
-        SolicitudAperturaPorContribucionRepository.getInstancia().getCantidadViandasPendientes(heladera);
+            SolicitudAperturaPorContribucionRepository.getInstancia().getCantidadViandasPendientes(heladera);
 
     return heladera.getCapacidadEnViandas() - getCantidadViandasDepositadas(heladera) - viandasEnContribucionesVigentes;
-  }
-
-  public int insert(Heladera heladera) throws RepositoryException {
-    if (get(heladera.getUbicacion()).isPresent()) {
-      throw new RepositoryException("Una heladera ya se encuentra en esa ubicación");
-    }
-
-    heladeras.add(heladera);
-    heladera.setId(heladeras.size());
-
-    return heladera.getId();
-  }
-
-  public void updateTiempoHeladera(int id, Heladera nuevaHeladera) {
-    Optional<Heladera> heladera = get(id);
-    heladera.ifPresent(value -> value.setUltimaTempRegistradaCelsius(
-        nuevaHeladera.getUltimaTempRegistradaCelsius())
-    );
-  }
-
-  public void deleteTodas() {
-    heladeras.clear();
   }
 }
 
