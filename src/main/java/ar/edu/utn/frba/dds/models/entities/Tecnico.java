f70ca5e0b3bdb9a98ccbf8878888d785f0cbe2d5
lavelazquez <lavelazquez@frba.utn.edu.ar>
2024-08-31 18:37:57 -0300

    Añadi mapeo a las entidades Tecnico y AreaGeografica con anotation hibernate.

--- src/main/java/ar/edu/utn/frba/dds/models/entities/Tecnico.java
+++ src/main/java/ar/edu/utn/frba/dds/models/entities/Tecnico.java
@@ -7,19 +7,45 @@ import ar.edu.utn.frba.dds.models.entities.ubicacion.CalculadoraDistancia;
 import ar.edu.utn.frba.dds.models.entities.users.Rol;
 import ar.edu.utn.frba.dds.models.entities.users.Usuario;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.List;
+import java.util.UUID;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Transient;
 
+@Entity
+@Table(name = "tecnico")
 @Getter
+@NoArgsConstructor
 public final class Tecnico {
   // TODO: el rol capaz debería estar almacenado en un repositorio?
+  @Column(name = "id")
+  @Id
+  @NonNull UUID id;
+  @Transient
   private static final Rol ROL_DEFAULT = new Rol("tecnico", new HashSet<>());
-  final @NonNull String cuil;
-  final @NonNull Usuario usuario;
+  @Column(name = "cuil", unique = true, nullable = false, updatable = false)
+  @NonNull String cuil;
+  
+  @OneToOne(cascade = CascadeType.ALL)
+  @MapsId
+  @JoinColumn(name = "idUsuario", referencedColumnName = "id")
+  @NonNull Usuario usuario; 
+  @Embedded
   @NonNull AreaGeografica areaAsignada;
 
   public Tecnico(@NonNull Documento documento,
--- src/main/java/ar/edu/utn/frba/dds/models/entities/ubicacion/AreaGeografica.java
+++ src/main/java/ar/edu/utn/frba/dds/models/entities/ubicacion/AreaGeografica.java
@@ -1,6 +1,8 @@
 package ar.edu.utn.frba.dds.models.entities.ubicacion;
 
 import lombok.NonNull;
+import javax.persistence.Embeddable;
 
+@Embeddable
 public record AreaGeografica(@NonNull CoordenadasGeograficas centro, float radioEnMetros) {
 }
