b5078b242893e33a0281fdd0b8eb400e5c62a6de
Guadalupe Hryb <ghrybalvarez@frba.utn.edu.ar>
2024-09-03 23:47:03 -0300

    corrección metodo hardcodeado

--- src/main/java/ar/edu/utn/frba/dds/models/repositories/documentacion/TarjetasRepository.java
+++ src/main/java/ar/edu/utn/frba/dds/models/repositories/documentacion/TarjetasRepository.java
@@ -5,13 +5,30 @@ import ar.edu.utn.frba.dds.models.entities.users.Usuario;
 import ar.edu.utn.frba.dds.models.repositories.HibernateEntityManager;
 
 
+import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import java.util.Optional;
 import java.util.UUID;
 
 public class TarjetasRepository extends HibernateEntityManager<Tarjeta, UUID> {
   public Optional<Tarjeta> getVigentePara(Usuario usuario) {
-    String criteria = "AND fecha_alta IS NOT NULL AND fecha_baja IS NULL";
-    return findFirstByCriteria("recipiente", usuario, criteria);
+    EntityManager em = entityManager();
+    CriteriaBuilder cb = em.getCriteriaBuilder();
+    CriteriaQuery<Tarjeta> query = cb.createQuery(Tarjeta.class);
+    Root<Tarjeta> root = query.from(Tarjeta.class);
+
+    Predicate recipientePredicate = cb.equal(root.get("recipiente"), usuario);
+    Predicate fechaAltaNotNullPredicate = cb.isNotNull(root.get("fechaAlta"));
+    Predicate fechaBajaNullPredicate = cb.isNull(root.get("fechaBaja"));
+
+    query.select(root)
+        .where(cb.and(recipientePredicate, fechaAltaNotNullPredicate, fechaBajaNullPredicate));
+
+    return em.createQuery(query).setMaxResults(1).getResultStream().findFirst();
   }
+
 
 }
