dc3459d2760506c50054faea2667854a700320ab
nicolaslazo <45973144+nicolaslazo@users.noreply.github.com>
2024-09-05 20:08:01 -0300

    Métodos redundantes en ViandasRepository eliminados

--- src/main/java/ar/edu/utn/frba/dds/models/repositories/ViandasRepository.java
+++ src/main/java/ar/edu/utn/frba/dds/models/repositories/ViandasRepository.java
@@ -10,12 +10,11 @@ import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
+import java.util.stream.Stream;
 
 public class ViandasRepository extends HibernateEntityManager<Vianda, Long> {
-  public List<Vianda> getAlmacenadas(Heladera heladera) {
-    //TODO: Cambiar el ID de Heladera en su issue
+  public Stream<Vianda> findAll(Heladera heladera) {
     EntityManager em = entityManager();
     CriteriaBuilder cb = em.getCriteriaBuilder();
     CriteriaQuery<Vianda> query = cb.createQuery(Vianda.class);
@@ -23,15 +22,15 @@ public class ViandasRepository extends HibernateEntityManager<Vianda, Long> {
 
     query.select(root).where(cb.equal(root.get("heladera"), heladera));
 
-    return em.createQuery(query).getResultList();
+    return em.createQuery(query).getResultStream();
   }
 
-  private void assertHeladeraTieneSuficienteEspacio(Heladera destino, int cantidadViandas) throws RepositoryException {
+  private void assertHeladeraTieneSuficienteEspacio(Heladera destino, int cantidadViandas) {
     final int capacidadDisponible = new HeladerasRepository().getCapacidadDisponible(destino);
     final String sPlural = cantidadViandas > 1 ? "s" : "";
 
     if (capacidadDisponible < cantidadViandas)
-      throw new RepositoryException(
+      throw new RuntimeException(
           "La heladera con id " +
               destino.getId() +
               " no posee el espacio para esta" +
@@ -44,13 +43,13 @@ public class ViandasRepository extends HibernateEntityManager<Vianda, Long> {
               capacidadDisponible);
   }
 
-  private void assertViandasSonDeLaMismaHeladera(Collection<Vianda> viandas) throws RepositoryException {
+  private void assertViandasSonDeLaMismaHeladera(Collection<Vianda> viandas) {
     final Set<Heladera> heladerasInvolucradas = new HashSet<>(viandas.stream().map(Vianda::getHeladera).toList());
     if (heladerasInvolucradas.size() > 1)
-      throw new RepositoryException("No se pueden insertar viandas de heladeras distintas en la misma transacción");
+      throw new RuntimeException("No se pueden insertar viandas de heladeras distintas en la misma transacción");
   }
 
-  public void insert(Collection<Vianda> viandas) throws RepositoryException {
+  public void insertAll(Collection<Vianda> viandas) {
     // Diseñada para calcular la disponibilidad de espacio en una heladera sola.
     // No hay caso de uso que justifique complicarla
     assertViandasSonDeLaMismaHeladera(viandas);
@@ -58,32 +57,17 @@ public class ViandasRepository extends HibernateEntityManager<Vianda, Long> {
     final Heladera heladeraInvolucrada = viandas.iterator().next().getHeladera();
     assertHeladeraTieneSuficienteEspacio(heladeraInvolucrada, viandas.size());
 
-    insertAll(viandas);
+    super.insertAll(viandas);
   }
 
-  /*
   public void insert(Vianda vianda) {
-    //TODO: Esto rompe aca, hay que agregar la logica de esto en el controller
-    // assertHeladeraTieneSuficienteEspacio(vianda.getHeladera(), 1);
+    assertHeladeraTieneSuficienteEspacio(vianda.getHeladera(), 1);
     super.insert(vianda);
   }
-  */
 
-  public void update(Vianda vianda) {
-    super.update(vianda);
-  }
-
-  public void updateUbicacion(Vianda vianda, Heladera ubicacionNueva) throws RepositoryException {
+  public void updateUbicacion(Vianda vianda, Heladera ubicacionNueva) {
     assertHeladeraTieneSuficienteEspacio(ubicacionNueva, 1);
     vianda.setHeladera(ubicacionNueva);
     update(vianda);
-  }
-
-  public void delete(Vianda vianda) {
-    super.delete(vianda);
-  }
-
-  public void deleteTodas() {
-    deleteAll();
   }
 }
