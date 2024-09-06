4806ce08befe09f3e8c9c8185213231bd215f6ce
nicolaslazo <45973144+nicolaslazo@users.noreply.github.com>
2024-09-05 23:00:08 -0300

    HibernatePersistenceReset usado

--- src/test/java/ar/edu/utn/frba/dds/models/repositories/RecompensasRepositoryTest.java
+++ src/test/java/ar/edu/utn/frba/dds/models/repositories/RecompensasRepositoryTest.java
@@ -6,9 +6,7 @@ import ar.edu.utn.frba.dds.models.entities.documentacion.Documento;
 import ar.edu.utn.frba.dds.models.entities.documentacion.TipoDocumento;
 import ar.edu.utn.frba.dds.models.entities.recompensas.Recompensa;
 import ar.edu.utn.frba.dds.models.entities.ubicacion.CoordenadasGeograficas;
-import ar.edu.utn.frba.dds.models.repositories.colaborador.ColaboradorRepository;
 import ar.edu.utn.frba.dds.models.repositories.recompensas.RecompensasRepository;
-import ar.edu.utn.frba.dds.models.repositories.users.UsuariosRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
@@ -34,9 +32,7 @@ class RecompensasRepositoryTest {
 
   @AfterEach
   void tearDown() {
-    new RecompensasRepository().deleteAll();
-    new ColaboradorRepository().deleteAll();
-    new UsuariosRepository().deleteAll();
+    new HibernatePersistenceReset().execute();
   }
 
   @Test
