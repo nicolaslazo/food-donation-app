87459fd56166fb8d70a19ea5453e524e4469af10
febenitez04 <febenitez@frba.utn.edu.ar>
2024-09-06 11:20:58 -0300

    ORMizacion CuidadoHeladera

--- src/main/java/ar/edu/utn/frba/dds/models/entities/contribucion/CuidadoHeladera.java
+++ src/main/java/ar/edu/utn/frba/dds/models/entities/contribucion/CuidadoHeladera.java
@@ -5,12 +5,24 @@ import ar.edu.utn.frba.dds.models.entities.heladera.Heladera;
 import lombok.Getter;
 import lombok.NonNull;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cuidadoHeladera")
 @Getter
 public class CuidadoHeladera extends Contribucion {
-  final Heladera heladera;
+
+  @ManyToOne
+  @JoinColumn(name = "idHeladera", referencedColumnName = "id")
+  Heladera heladera;
 
   public CuidadoHeladera(@NonNull Colaborador colaborador, Heladera heladera) {
     super(colaborador);
     this.heladera = heladera;
   }
+
+  protected CuidadoHeladera() {}
 }
