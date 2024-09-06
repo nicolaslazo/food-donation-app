721137e2e31f537728c211e8f650f61910c6f3ae
Guadalupe Hryb <ghrybalvarez@frba.utn.edu.ar>
2024-09-06 13:36:51 -0300

    Correcciones y tests que andan bien

--- src/test/java/ar/edu/utn/frba/dds/models/repositories/heladera/MovimientosHeladeraRepositoryTest.java
+++ src/test/java/ar/edu/utn/frba/dds/models/repositories/heladera/MovimientosHeladeraRepositoryTest.java
@@ -1,23 +1,47 @@
 package ar.edu.utn.frba.dds.models.repositories.heladera;
 
 
+import ar.edu.utn.frba.dds.models.entities.colaborador.Colaborador;
+import ar.edu.utn.frba.dds.models.entities.documentacion.Documento;
+import ar.edu.utn.frba.dds.models.entities.documentacion.TipoDocumento;
 import ar.edu.utn.frba.dds.models.entities.heladera.EventoMovimiento;
+import ar.edu.utn.frba.dds.models.entities.heladera.Heladera;
+import ar.edu.utn.frba.dds.models.entities.ubicacion.CoordenadasGeograficas;
 import ar.edu.utn.frba.dds.models.repositories.HibernatePersistenceReset;
+import ar.edu.utn.frba.dds.models.repositories.colaborador.ColaboradorRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.mock;
+import java.time.LocalDate;
+import java.time.ZonedDateTime;
 
 class MovimientosHeladeraRepositoryTest {
   MovimientosHeladeraRepository repositorio = new MovimientosHeladeraRepository();
-  EventoMovimiento heladeraMock = mock(EventoMovimiento.class);
+  CoordenadasGeograficas obelisco = new CoordenadasGeograficas(-34.5611745, -58.4287506);
+
+  Colaborador colaborador = new Colaborador(
+      new Documento(TipoDocumento.DNI, 1),
+      "",
+      "",
+      LocalDate.now(),
+      new CoordenadasGeograficas(-30d, -50d));
+  Heladera heladera = new Heladera("Una heladera",
+      obelisco,
+      colaborador,
+      50,
+      ZonedDateTime.now().minusMonths(5)
+  );
+
+  EventoMovimiento eventoMovimiento = new EventoMovimiento(heladera, ZonedDateTime.now());
   
   @BeforeEach
   void setUp() {
-    repositorio.insert(heladeraMock);
+    new ColaboradorRepository().insert(colaborador);
+    new HeladerasRepository().insert(heladera);
+    repositorio.insert(eventoMovimiento);
   }
 
   @AfterEach
@@ -31,7 +55,7 @@ class MovimientosHeladeraRepositoryTest {
 
   @Test
   void testInsert() {
-    assertNotNull(heladeraMock.getId());
+    assertNotNull(eventoMovimiento.getId());
   }
 
 }
