0e4fe3ac4a0e453c8e01094a100fb48a8a0694d3
nicolaslazo <45973144+nicolaslazo@users.noreply.github.com>
2024-09-06 02:06:35 -0300

    Propagación de cambios, un test rompe

--- src/test/java/ar/edu/utn/frba/dds/models/repositories/contacto/SuscripcionRepositoryTest.java
+++ src/test/java/ar/edu/utn/frba/dds/models/repositories/contacto/SuscripcionRepositoryTest.java
@@ -3,64 +3,84 @@ package ar.edu.utn.frba.dds.models.repositories.contacto;
 import ar.edu.utn.frba.dds.models.entities.colaborador.Colaborador;
 import ar.edu.utn.frba.dds.models.entities.contacto.Suscripcion;
 import ar.edu.utn.frba.dds.models.entities.contribucion.MotivoDeDistribucion;
+import ar.edu.utn.frba.dds.models.entities.documentacion.Documento;
+import ar.edu.utn.frba.dds.models.entities.documentacion.TipoDocumento;
 import ar.edu.utn.frba.dds.models.entities.heladera.Heladera;
-import ar.edu.utn.frba.dds.models.repositories.RepositoryException;
-import ar.edu.utn.frba.dds.models.repositories.heladera.HeladerasRepository;
+import ar.edu.utn.frba.dds.models.entities.ubicacion.CoordenadasGeograficas;
+import ar.edu.utn.frba.dds.models.repositories.HibernatePersistenceReset;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.MockedConstruction;
 
+import java.time.LocalDate;
+import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.mockConstruction;
-import static org.mockito.Mockito.when;
 
 class SuscripcionRepositoryTest {
-  final SuscripcionRepository repositorio = SuscripcionRepository.getInstancia();
-  final Heladera heladeraMock = mock(Heladera.class);
-  final Colaborador colaboradorMock = mock(Colaborador.class);
+  final SuscripcionRepository repositorio = new SuscripcionRepository();
+  final Colaborador colaborador = new Colaborador(new Documento(TipoDocumento.DNI, 1),
+      "",
+      "",
+      LocalDate.now(),
+      null);
+  final Heladera heladera = new Heladera("",
+      new CoordenadasGeograficas(-34d, -58d),
+      colaborador,
+      5,
+      ZonedDateTime.now());
   final Suscripcion suscripcion =
-      new Suscripcion(heladeraMock, MotivoDeDistribucion.FALLA_HELADERA, null, colaboradorMock);
+      new Suscripcion(colaborador, heladera, MotivoDeDistribucion.FALLA_HELADERA, null);
 
   @BeforeEach
-  void setUp() throws RepositoryException {
+  void setUp() {
     repositorio.insert(suscripcion);
   }
 
   @AfterEach
   void tearDown() {
-    repositorio.deleteTodas();
+    new HibernatePersistenceReset().execute();
   }
 
   @Test
   void testGetPorId() {
-    Optional<Suscripcion> encontrada = repositorio.get(1);
+    Optional<Suscripcion> encontrada = repositorio.findById(suscripcion.getId());
 
-    assertTrue(encontrada.isPresent());
-    assertEquals(1, encontrada.get().getId());
+    assertEquals(suscripcion.getId(), encontrada.get().getId());
   }
 
   @Test
-  void testGetSuscripcionesRelevantesAStockDeHeladera() throws RepositoryException {
-    Heladera heladeraMock = mock(Heladera.class);
-    when(heladeraMock.getCapacidadEnViandas()).thenReturn(5);
+  void testGetSuscripcionesRelevantesAStockDeHeladera() {
+    final Colaborador otroColaborador = new Colaborador(new Documento(TipoDocumento.DNI, 2),
+        "",
+        "",
+        LocalDate.now(),
+        null);
+    final Colaborador otroColaboradorMas = new Colaborador(new Documento(TipoDocumento.DNI, 3),
+        "",
+        "",
+        LocalDate.now(),
+        null);
+    final Colaborador todaviaOtroColaboradorMas = new Colaborador(new Documento(TipoDocumento.DNI, 4),
+        "",
+        "",
+        LocalDate.now(),
+        null);
 
     Suscripcion faltanViandasDeseada =
-        new Suscripcion(heladeraMock, MotivoDeDistribucion.FALTAN_VIANDAS, 4, mock(Colaborador.class));
+        new Suscripcion(colaborador, heladera, MotivoDeDistribucion.FALTAN_VIANDAS, 4);
     Suscripcion faltanViandasIndeseada =
-        new Suscripcion(heladeraMock, MotivoDeDistribucion.FALTAN_VIANDAS, 2, mock(Colaborador.class));
+        new Suscripcion(otroColaborador, heladera, MotivoDeDistribucion.FALTAN_VIANDAS, 2);
     Suscripcion faltaEspacioDeseada =
-        new Suscripcion(heladeraMock, MotivoDeDistribucion.FALTA_ESPACIO, 4, mock(Colaborador.class));
+        new Suscripcion(otroColaboradorMas, heladera, MotivoDeDistribucion.FALTA_ESPACIO, 4);
     Suscripcion faltaEspacioIndeseada =
-        new Suscripcion(heladeraMock, MotivoDeDistribucion.FALTA_ESPACIO, 2, mock(Colaborador.class));
+        new Suscripcion(todaviaOtroColaboradorMas, heladera, MotivoDeDistribucion.FALTA_ESPACIO, 2);
 
     for (Suscripcion suscripcion :
         List.of(faltanViandasDeseada, faltanViandasIndeseada, faltaEspacioDeseada, faltaEspacioIndeseada)) {
@@ -69,33 +89,33 @@ class SuscripcionRepositoryTest {
 
     Set<Suscripcion> interesadas;
 
-    try (MockedConstruction<HeladerasRepository> ignored =
-             mockConstruction(HeladerasRepository.class, (mock, context) ->
-                 when(mock.getCantidadViandasDepositadas(heladeraMock)).thenReturn(3L))) {
-      interesadas = repositorio.getInteresadasEnStock(heladeraMock).collect(Collectors.toSet());
-    }
+//    try (MockedConstruction<HeladerasRepository> ignored =
+//             mockConstruction(HeladerasRepository.class, (mock, context) ->
+//                 when(mock.getCantidadViandasDepositadas(heladera)).thenReturn(3L))) {
+    interesadas = repositorio.findInteresadasEnStock(heladera).collect(Collectors.toSet());
+//    }
 
     assertEquals(Set.of(faltanViandasDeseada, faltaEspacioDeseada), interesadas);
   }
 
   @Test
   void testInsertarSuscripcion() {
-    assertEquals(1, suscripcion.getId());
+    assertNotNull(suscripcion.getId());
   }
 
   @Test
-  void testInsertarSuscripcionDuplicadaLanzaExcepcion() throws RepositoryException {
-    repositorio.insert(new Suscripcion(heladeraMock, MotivoDeDistribucion.FALTAN_VIANDAS, 4, colaboradorMock));
+  void testInsertarSuscripcionDuplicadaLanzaExcepcion() {
+    repositorio.insert(new Suscripcion(colaborador, heladera, MotivoDeDistribucion.FALTAN_VIANDAS, 4));
 
-    assertThrows(RepositoryException.class,
+    assertThrows(RuntimeException.class,
         () -> repositorio.insert(
-            new Suscripcion(heladeraMock, MotivoDeDistribucion.FALTAN_VIANDAS, 6, colaboradorMock)));
+            new Suscripcion(colaborador, heladera, MotivoDeDistribucion.FALTAN_VIANDAS, 6)));
   }
 
   @Test
   void testEliminarTodas() {
-    repositorio.deleteTodas();
+    repositorio.deleteAll();
 
-    assertTrue(repositorio.get(1).isEmpty());
+    assertEquals(0, repositorio.findAll().count());
   }
 }
