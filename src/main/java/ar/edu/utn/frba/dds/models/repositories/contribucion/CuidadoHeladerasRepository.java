04ec8aedc5339f21f8ed40d7560d9789f3cb4f89
febenitez04 <febenitez@frba.utn.edu.ar>
2024-09-06 11:22:01 -0300

    CuidadoHeladerasRepository extends Hibernate

--- src/main/java/ar/edu/utn/frba/dds/models/repositories/contribucion/CuidadoHeladerasRepository.java
+++ src/main/java/ar/edu/utn/frba/dds/models/repositories/contribucion/CuidadoHeladerasRepository.java
@@ -2,53 +2,25 @@ package ar.edu.utn.frba.dds.models.repositories.contribucion;
 
 import ar.edu.utn.frba.dds.models.entities.colaborador.Colaborador;
 import ar.edu.utn.frba.dds.models.entities.contribucion.CuidadoHeladera;
+import ar.edu.utn.frba.dds.models.repositories.HibernateEntityManager;
 import ar.edu.utn.frba.dds.models.repositories.RepositoryException;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class CuidadoHeladerasRepository {
-  static CuidadoHeladerasRepository instancia = null;
-  final List<CuidadoHeladera> contribuciones;
-
-  public CuidadoHeladerasRepository() {
-    contribuciones = new ArrayList<>();
-  }
-
-  public static CuidadoHeladerasRepository getInstancia() {
-    if (instancia == null) {
-      instancia = new CuidadoHeladerasRepository();
-    }
-
-    return instancia;
-  }
-
-  public Optional<CuidadoHeladera> get(Long id) {
-    return contribuciones.stream().filter(contribucion -> contribucion.getId() == id).findFirst();
-  }
-
-  public Long insert(CuidadoHeladera contribucion) throws RepositoryException {
-    if (contribuciones.stream().map(CuidadoHeladera::getHeladera).anyMatch(heladera -> heladera == contribucion.getHeladera())) {
-      throw new RepositoryException("Esa heladera ya fue insertada en otra contribución");
-    }
-
-    contribuciones.add(contribucion);
-    contribucion.setId((long) contribuciones.size());
-
-    return contribucion.getId();
-  }
-
+public class CuidadoHeladerasRepository extends HibernateEntityManager<CuidadoHeladera, Long> {
   public int getMesesActivosCumulativos(Colaborador colaborador) {
-    return contribuciones
-            .stream()
-            .filter(contribucion -> contribucion.getColaborador() == colaborador)
-            .mapToInt(contribucion -> contribucion.getHeladera().mesesActiva())
-            .sum();
-  }
+    /* Hago la diferencia entre los meses y años, multiplico el año por 12 para tener los meses */
+    String jpql = "SELECT SUM(MONTH(CURRENT_DATE) - MONTH(ch.heladera.fechaInstalacion) " +
+            "+ 12 * (YEAR(CURRENT_DATE) - YEAR(ch.heladera.fechaInstalacion))) " +
+            "FROM CuidadoHeladera ch " +
+            "WHERE ch.colaborador = :colaborador";
 
-
-  public void deleteTodas() {
-    contribuciones.clear();
+    return entityManager()
+            .createQuery(jpql, Long.class)
+            .setParameter("colaborador", colaborador)
+            .getSingleResult()
+            .intValue();
   }
 }
