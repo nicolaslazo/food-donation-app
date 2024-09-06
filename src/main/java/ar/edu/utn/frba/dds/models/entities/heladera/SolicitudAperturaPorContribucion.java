52d87751138a7441ca6b17fd07118d0bd3906955
Adrian <adtolaba@frba.utn.edu.ar>
2024-09-06 18:39:24 -0300

    Cambio para el Checkstyle

--- src/main/java/ar/edu/utn/frba/dds/models/entities/heladera/SolicitudAperturaPorContribucion.java
+++ src/main/java/ar/edu/utn/frba/dds/models/entities/heladera/SolicitudAperturaPorContribucion.java
@@ -10,7 +10,12 @@ import lombok.Getter;
 import lombok.NonNull;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Transient;
 import java.time.ZonedDateTime;
 import java.util.Collection;
 import java.util.Optional;
