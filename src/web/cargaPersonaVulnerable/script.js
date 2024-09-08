4be1200f4e65aabc17c6b31fe772da9d17188f46
febenitez04 <febenitez@frba.utn.edu.ar>
2024-09-08 15:23:16 -0300

    Arreglo de form de personaVulnerable.html + Correciones

--- src/web/cargaPersonaVulnerable/script.js
+++ src/web/cargaPersonaVulnerable/script.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-
     // Validar número de menores a cargo
     const menoresCargoInput = document.getElementById('menores-cargo');
     menoresCargoInput.addEventListener('input', function() {
@@ -9,44 +8,40 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
-    // Validar que el ID de la tarjeta sea un UUID
+    // Validar que el ID de la tarjeta sea un UUID cuando el campo pierde el foco
     const tarjetaInput = document.getElementById('tarjeta');
-    tarjetaInput.addEventListener('input', function() {
+    tarjetaInput.addEventListener('blur', function() {
         const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i;
-        if (!uuidRegex.test(tarjetaInput.value)) {
+        if (tarjetaInput.value && !uuidRegex.test(tarjetaInput.value)) {
             alert('El código de la tarjeta debe ser un UUID válido.');
             tarjetaInput.value = ''; // Resetea el valor
         }
     });
 
-    const addressSection = document.getElementById('direccion'); // El div que contiene los campos de la dirección
-    const tieneDomicilioCheckbox = document.getElementById('tiene-domicilio'); // Nueva checkbox
+    const addressSection = document.getElementById('direccion');
+    const tieneDomicilioCheckbox = document.getElementById('tiene-domicilio');
 
-    // Manejar la visibilidad de los campos de Domicilio cuando se selecciona la nueva checkbox
     tieneDomicilioCheckbox.addEventListener('change', function() {
         if (this.checked) {
-            addressSection.classList.add('show'); // Muestra el div de la dirección
-            addressSection.classList.remove('hidden'); // Asegura que la clase 'hidden' no esté presente
+            addressSection.classList.add('show');
+            addressSection.classList.remove('hidden');
         } else {
-            addressSection.classList.add('hidden'); // Oculta el div de la dirección
-            addressSection.classList.remove('show'); // Asegura que la clase 'show' no esté presente
+            addressSection.classList.add('hidden');
+            addressSection.classList.remove('show');
         }
     });
 
+    const documentSection = document.getElementById('posee-documento');
+    const tieneDocumentoCheckbox = document.getElementById('tiene-dni');
 
-    const documentSection = document.getElementById('posee-documento'); // El div que contiene los campos de la dirección
-    const tieneDocumentoCheckbox = document.getElementById('tiene-dni'); // Nueva checkbox
-
-    // Manejar la visibilidad de los campos de Domicilio cuando se selecciona la nueva checkbox
     tieneDocumentoCheckbox.addEventListener('change', function() {
         if (this.checked) {
-            documentSection.classList.add('show'); // Muestra el div de la dirección
-            documentSection.classList.remove('hidden'); // Asegura que la clase 'hidden' no esté presente
+            documentSection.classList.add('show');
+            documentSection.classList.remove('hidden');
         } else {
-            documentSection.classList.add('hidden'); // Oculta el div de la dirección
-            documentSection.classList.remove('show'); // Asegura que la clase 'show' no esté presente
+            documentSection.classList.add('hidden');
+            documentSection.classList.remove('show');
         }
     });
 
 });
-
