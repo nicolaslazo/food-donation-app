b91e35f2c789a7690d386bac373276cc03ea8a17
Guadalupe Hryb <ghrybalvarez@frba.utn.edu.ar>
2024-09-08 22:40:37 -0300

    repo

--- src/main/java/ar/edu/utn/frba/dds/models/entities/heladera/SolicitudAperturaPorConsumicion.java
+++ src/main/java/ar/edu/utn/frba/dds/models/entities/heladera/SolicitudAperturaPorConsumicion.java
@@ -3,15 +3,41 @@ package ar.edu.utn.frba.dds.models.entities.heladera;
 import ar.edu.utn.frba.dds.config.ConfigLoader;
 import ar.edu.utn.frba.dds.models.entities.Vianda;
 import ar.edu.utn.frba.dds.models.entities.documentacion.Tarjeta;
+import lombok.Getter;
 import lombok.NonNull;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import java.time.ZonedDateTime;
 
+@Entity
+@Table(name = "solicitudAperturaPorConsumicion")
 public class SolicitudAperturaPorConsumicion {
+
+  @GeneratedValue
+  @Id
+  Long id;
+
+  @ManyToOne
+  @JoinColumn(name = "trjeta_id", nullable = false)
   @NonNull Tarjeta tarjeta;
+
+  @ManyToOne
+  @JoinColumn(name = "vianda_id", nullable = false, referencedColumnName = "id")
   @NonNull Vianda vianda;
+
+  @Column(name = "fechaCreacion", nullable = false)
   @NonNull ZonedDateTime fechaCreacion;
+
+  @Column(name = "fechaVencimiento", nullable = false)
   @NonNull ZonedDateTime fechaVencimiento;
+
+  @Column(name = "fechaUsada")
   ZonedDateTime fechaUsada = null;
 
   public SolicitudAperturaPorConsumicion(@NonNull Tarjeta tarjeta,
---
+++ src/main/java/ar/edu/utn/frba/dds/models/repositories/heladera/SolicitudAperturaPorConsumicionRepository.java
@@ -0,0 +1,7 @@
+package ar.edu.utn.frba.dds.models.repositories.heladera;
+
+import ar.edu.utn.frba.dds.models.entities.heladera.SolicitudAperturaPorConsumicion;
+import ar.edu.utn.frba.dds.models.repositories.HibernateEntityManager;
+
+public class SolicitudAperturaPorConsumicionRepository extends HibernateEntityManager<SolicitudAperturaPorConsumicion, Long> {
+}
