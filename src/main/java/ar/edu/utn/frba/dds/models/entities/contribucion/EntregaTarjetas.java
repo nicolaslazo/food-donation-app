309d262b8fe3d7862a1a57e37a8de136ce1f7b53
febenitez04 <febenitez@frba.utn.edu.ar>
2024-09-09 09:52:05 -0300

    ORM EntregaTarjetas

--- src/main/java/ar/edu/utn/frba/dds/models/entities/contribucion/EntregaTarjetas.java
+++ src/main/java/ar/edu/utn/frba/dds/models/entities/contribucion/EntregaTarjetas.java
@@ -4,18 +4,27 @@ import ar.edu.utn.frba.dds.models.entities.colaborador.Colaborador;
 import ar.edu.utn.frba.dds.models.entities.documentacion.Tarjeta;
 import lombok.Getter;
 import lombok.NonNull;
-import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.util.List;
 
+@Entity
+@Table(name = "entregaTarjetas")
 @Getter
 public class EntregaTarjetas extends Contribucion {
-  final List<Tarjeta> tarjetasRepartidas;
+  @OneToMany
+  @JoinColumn(name = "idEntrega", referencedColumnName = "id")
+  List<Tarjeta> tarjetasRepartidas;
 
   public EntregaTarjetas(@NonNull Colaborador colaborador, @NonNull List<Tarjeta> tarjetas) {
     super(colaborador);
     this.tarjetasRepartidas = tarjetas;
   }
+
+  protected EntregaTarjetas() {}
 
   @Override
   public String toString() {
