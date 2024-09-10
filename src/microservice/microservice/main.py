540c44d912a916e8c17b8a1827064f44dabbf556
nicolaslazo <45973144+nicolaslazo@users.noreply.github.com>
2024-09-09 23:21:18 -0300

    Query final hecha

--- src/microservice/microservice/main.py
+++ src/microservice/microservice/main.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 from db import get_db
 from fastapi import Depends, FastAPI
 from models import Heladera, SolicitudAperturaPorConsumicion, Tarjeta, Usuario
@@ -6,44 +8,25 @@ from sqlalchemy.orm import Session
 app = FastAPI()
 
 
-@app.get("/")
-def hello_world():
-    return {"Hello": "World"}
-
-
-@app.get("/usuarios")
-def usuarios(db: Session = Depends(get_db)):
-    return db.query(Usuario).all()
-
-
-@app.get("/solicitudes")
-def solicitudes(db: Session = Depends(get_db)):
-    return db.query(SolicitudAperturaPorConsumicion).all()
-
-
-@app.get("/heladeras")
-def heladeras(db: Session = Depends(get_db)):
-    return db.query(Heladera).all()
-
-
-# @app.get("/personas-por-barrio", response_model=schemas.ReporteBarrios)
 @app.get("/personas-por-barrio")
 def personas_por_barrio(db: Session = Depends(get_db)):
     resultados = (
-        db.query(SolicitudAperturaPorConsumicion, Tarjeta, Usuario, Heladera)
+        db.query(Heladera.barrio, Usuario.primerNombre, Usuario.apellido)
         .filter(SolicitudAperturaPorConsumicion.fechaUsada.isnot(None))
+        .join(
+            SolicitudAperturaPorConsumicion,
+            SolicitudAperturaPorConsumicion.idHeladera == Heladera.id,
+        )
         .join(Tarjeta, Tarjeta.id == SolicitudAperturaPorConsumicion.idTarjeta)
         .join(Usuario, Usuario.id == Tarjeta.idRecipiente)
-        .join(Heladera, Heladera.id == SolicitudAperturaPorConsumicion.idHeladera)
         .all()
     )
 
+    agrupados = defaultdict(list)
+    for barrio, primer_nombre, apellido in resultados:
+        agrupados[barrio].append(f"{primer_nombre} {apellido}")
+
     return [
-        {
-            "solicitud": solicitud.as_dict(),
-            "tarjeta": tarjeta.as_dict(),
-            "usuario": usuario.as_dict(),
-            "heladera": heladera.as_dict(),
-        }
-        for solicitud, tarjeta, usuario, heladera in resultados
+        {"barrio": barrio, "cantidad": len(nombres), "nombres": nombres}
+        for barrio, nombres in agrupados.items()
     ]
