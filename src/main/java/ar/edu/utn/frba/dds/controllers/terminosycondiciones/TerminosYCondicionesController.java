6423e2ad5e5b88a15652747d7d8c810bb007274d
febenitez04 <febenitez@frba.utn.edu.ar>
2024-10-01 22:26:17 -0300

    Preparo PR para futuro merge

--- src/main/java/ar/edu/utn/frba/dds/controllers/terminosycondiciones/TerminosYCondicionesController.java
+++ src/main/java/ar/edu/utn/frba/dds/controllers/terminosycondiciones/TerminosYCondicionesController.java
@@ -4,12 +4,6 @@ import io.javalin.http.Context;
 
 public class TerminosYCondicionesController {
   public void index(Context context) {
-    System.out.println("Intento de renderizar");
-    try {
-      context.render("terminos/terminos.hbs");
-    } catch (Exception e) {
-      e.printStackTrace();
-      context.status(500).result("Error: " + e.getMessage());
-    }
+    context.render("terminos/terminos.hbs");
   }
 }
