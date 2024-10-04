82b42b0fcf1c4fd42956cf7fcd04b99a63734cdd
Guadalupe Hryb <ghrybalvarez@frba.utn.edu.ar>
2024-10-04 16:50:50 -0300

    Template, ruta y todo

---
+++ src/main/java/ar/edu/utn/frba/dds/controllers/cargacsv/CargaCSVController.java
@@ -0,0 +1,10 @@
+package ar.edu.utn.frba.dds.controllers.cargacsv;
+
+import io.javalin.http.Context;
+
+public class CargaCSVController {
+  public void index(Context context) {
+    context.render("quieroayudar/quieroayudar.hbs");
+  }
+}
+
