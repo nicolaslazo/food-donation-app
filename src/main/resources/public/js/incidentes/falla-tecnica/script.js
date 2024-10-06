8fda0f2924e94fa75306b759a610e63404771ffd
Adrian <adtolaba@frba.utn.edu.ar>
2024-10-05 21:41:30 -0300

    Se agrego que el mapa de Leafet integre las heladeras desde la base de datos

--- src/main/resources/public/js/incidentes/falla-tecnica/script.js
+++ src/main/resources/public/js/incidentes/falla-tecnica/script.js
@@ -4,17 +4,16 @@ const map = L.map('map', {
     zoom: 17
 });
 
+
 L.tileLayer(
     'https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',
     { attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' }
 ).addTo(map);
 
 // Marcadores para las heladeras
-L.marker([-34.6025246,-58.3843585], { title: 'Primera heladera' }).bindPopup('Primera heladera').openPopup().addTo(map);
-L.marker([-34.6024605,-58.3812774], { title: 'Segunda heladera' }).bindPopup('Segunda heladera').openPopup().addTo(map);
-L.marker([-34.6047476,-58.3795161], { title: 'Tercera heladera' }).bindPopup('Tercera heladera').openPopup().addTo(map);
-L.marker([-34.598607968791924, -58.420107873800205], {title: 'Heladera Medrano' }).bindPopup('Heladera medrano').openPopup().addTo(map);
-
+heladeras.forEach(function (heladera) {
+   L.marker([heladera.lat, heladera.long], {title: heladera.nombre}).bindPopup(heladera.nombre).openPopup().addTo(map);
+});
 var marker;
 
 map.on('click', function(e) {
@@ -36,5 +35,5 @@ L.control.pinSearch({
         console.log('Buscando:', query);
     },
     focusOnMarker: true,
-    maxSearchResults: 3
+    maxSearchResults: 6
 }).addTo(map);
