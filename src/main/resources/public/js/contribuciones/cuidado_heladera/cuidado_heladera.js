9dc65de0c820d9fca418393f969c5c7f13ed1e4f
febenitez04 <febenitez@frba.utn.edu.ar>
2024-10-11 16:24:41 -0300

    estaticos movidos

--- src/main/resources/public/js/contribuciones/cuidado_heladera/cuidado_heladera.js
+++ src/main/resources/public/js/contribuciones/cuidado_heladera/cuidado_heladera.js
@@ -5,6 +5,15 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 18,
 }).addTo(map);
 
+// Marcadores para las heladeras
+heladeras.forEach(function (heladera) {
+    var marcador = L.marker([heladera.lat, heladera.long], {title: heladera.nombre, id: heladera.idHeladera}).bindPopup(heladera.nombre).openPopup().addTo(map);
+    marcador.on('click', function (e){
+        document.getElementById("idHeladera").value = marcador.options.id;
+    })
+});
+
+// Definir la variable marker fuera del evento
 var marker;
 
 map.on('click', function(e) {
@@ -15,26 +24,28 @@ map.on('click', function(e) {
     var heladeraName = document.getElementById('heladera-name').value || 'Nueva heladera';
 
     if (marker) {
+        // Actualizar la posición del marcador existente
         marker.setLatLng(e.latlng);
     } else {
-        L.marker(e.latlng).bindPopup(heladeraName).openPopup().addTo(map);
+        // Crear un nuevo marcador
+        marker = L.marker(e.latlng).addTo(map);
     }
 
+    // Actualizar el popup del marcador con el nombre de la heladera
+    marker.bindPopup(heladeraName).openPopup();
+
+    // Actualizar los inputs de latitud y longitud
     document.getElementById('latitude').value = lat;
     document.getElementById('longitude').value = lng;
 });
 
 
-L.marker([-34.6025246,-58.3843585]).bindPopup('Primera heladera').openPopup().addTo(map);
-L.marker([-34.6024605,-58.3812774]).bindPopup('Segunda heladera').openPopup().addTo(map);
-L.marker([-34.6047476,-58.3795161]).bindPopup('Tercera heladera').openPopup().addTo(map);
-L.marker([-34.598607968791924, -58.420107873800205], {title: 'Heladera Medrano' }).bindPopup('Heladera medrano').openPopup().addTo(map);
-
 // Integración de Leaflet.PinSearch
-L.control.pinSearch({
+let pinSearchControl = L.control.pinSearch({
     placeholder: 'Buscar heladera...',
     onSearch: function(query) {
         console.log('Buscando:', query);
+        document.getElementById("idHeladera").value = pinSearchControl._findMarkerByTitle(query).options.id;
     },
     focusOnMarker: true,
     maxSearchResults: 3
