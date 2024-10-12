db974344c063af80220114b4eee8e60d85dd70f2
febenitez04 <febenitez@frba.utn.edu.ar>
2024-10-12 17:51:39 -0300

    AuthMiddleware implementado

--- src/main/java/ar/edu/utn/frba/dds/server/Router.java
+++ src/main/java/ar/edu/utn/frba/dds/server/Router.java
@@ -12,6 +12,7 @@ import ar.edu.utn.frba.dds.controllers.session.SessionController;
 import ar.edu.utn.frba.dds.controllers.terminosycondiciones.TerminosYCondicionesController;
 import ar.edu.utn.frba.dds.models.entities.users.Permiso;
 import ar.edu.utn.frba.dds.models.repositories.users.PermisosRepository;
+import ar.edu.utn.frba.dds.server.middleware.AuthMiddleware;
 import io.javalin.Javalin;
 
 import java.util.Arrays;
@@ -22,25 +23,38 @@ public class Router {
     PermisosRepository permisosRepository = new PermisosRepository();
     Permiso permisoAsignarTarjetas = permisosRepository.findByName("Asignar-Tarjetas").get();
 
-    app.get("/prueba", ctx -> ctx.result("Hola mundo!"));
+    app.before(ctx -> new SessionController().sessionInfo(ctx));
 
+    // --- Ruta Públicas ---
     app.get("/colaborador/login", new SessionController()::index);
     app.post("/colaborador/login", new SessionController()::create);
+    app.post("/colaborador/logout", new SessionController()::delete);
 
     app.get("/", new HomeController()::index);
     app.get("/terminos-y-condiciones", new TerminosYCondicionesController()::index);
     app.get("/quiero-ayudar", new QuieroAyudarController()::index);
-    app.get("/carga-csv", new CargaCSVController()::index);
     app.get("/formas-colaboracion", new FormasColaboracionController()::index);
 
     app.get("/colaborador/registro", new ColaboradorController()::index);
     app.post("/colaborador/registro", new ColaboradorController()::create);
+
+    // --- Rutas Protegidas que requieren autenticación ---
+    // Incidente/*
+    app.before("incidentes/*", new AuthMiddleware());
+    app.get("/incidentes/reportar-falla", IncidenteController.getInstancia()::index);
+
+    // Contribucion/*
+    app.before("/contribucion/*", new AuthMiddleware());
+    app.get("/contribucion/donacion-dinero", new DonacionDineroController()::index);
+
+    // Registro Persona Vulnerable
+    app.before("/persona-vulnerable/registro", new AuthMiddleware());
     app.get("/persona-vulnerable/registro", new PersonaVulnerableController()::index, permisoAsignarTarjetas);
     app.post("/persona-vulnerable/registro", new PersonaVulnerableController()::create, permisoAsignarTarjetas);
 
-    app.get("/incidentes/reportar-falla", IncidenteController.getInstancia()::index);
-
-    app.get("/contribuciones/donacion-dinero", new DonacionDineroController()::index);
+    // Carga CSV
+    app.before("/carga-csv", new AuthMiddleware());
+    app.get("/carga-csv", new CargaCSVController()::index);
 
     app.exception(Exception.class, (e, ctx) -> {
       ctx.status(500);
