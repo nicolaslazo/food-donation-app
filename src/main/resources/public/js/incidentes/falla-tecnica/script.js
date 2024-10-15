a4c5122ccc00194eb7801596bec01b00a1bdc0a3
febenitez04 <febenitez@frba.utn.edu.ar>
2024-10-15 10:58:49 -0300

    Vista falla-tecnica.hbs arreglada

--- src/main/resources/public/js/incidentes/falla-tecnica/script.js
+++ src/main/resources/public/js/incidentes/falla-tecnica/script.js
@@ -4,7 +4,6 @@ const map = L.map('map', {
     zoom: 17
 });
 
-
 L.tileLayer(
     'https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',
     { attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' }
@@ -37,3 +36,33 @@ let pinSearchControl = L.control.pinSearch({
     focusOnMarker: true,
     maxSearchResults: 3
 }).addTo(map);
+
+// Verificación de la Imagen Ingresada
+document.addEventListener('DOMContentLoaded', function() {
+    const fileInput = document.querySelector('input[name="imagen"]');
+    const form = document.querySelector('form');
+    const submitButton = document.querySelector('.submit-button input[type="submit"]');
+
+    fileInput.addEventListener('change', function(event) {
+        const file = event.target.files[0];
+        if (file) {
+            const fileType = file.type;
+            if (fileType !== 'image/jpeg') {
+                alert('Por favor, sube una imagen en formato JPG.');
+                fileInput.value = ''; // Limpiar el input
+                submitButton.disabled = true; // Deshabilitar botón de enviar
+            } else {
+                alert('Imagen aceptada.');
+                submitButton.disabled = false; // Habilitar botón de enviar
+            }
+        }
+    });
+
+    form.addEventListener('submit', function(event) {
+        const file = fileInput.files[0];
+        if (!file || file.type !== 'image/jpeg') {
+            event.preventDefault();
+            alert('Por favor, asegúrate de subir una imagen en formato JPG antes de enviar.');
+        }
+    });
+});
