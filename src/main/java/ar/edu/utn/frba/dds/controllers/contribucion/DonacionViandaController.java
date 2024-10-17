89ebd96c732c7e26cbb13e33e2a6c9da1ee35985
febenitez04 <febenitez@frba.utn.edu.ar>
2024-10-17 16:40:04 -0300

    Ruta + Controller de DonacionVianda

--- src/main/java/ar/edu/utn/frba/dds/controllers/contribucion/DonacionViandaController.java
+++ src/main/java/ar/edu/utn/frba/dds/controllers/contribucion/DonacionViandaController.java
@@ -1,21 +1,124 @@
 package ar.edu.utn.frba.dds.controllers.contribucion;
 
+import ar.edu.utn.frba.dds.dtos.input.contribucion.DonacionViandaInputDTO;
+import ar.edu.utn.frba.dds.models.entities.Vianda;
+import ar.edu.utn.frba.dds.models.entities.colaborador.Colaborador;
+import ar.edu.utn.frba.dds.models.entities.contribucion.DonacionViandas;
 import ar.edu.utn.frba.dds.models.entities.heladera.Heladera;
+import ar.edu.utn.frba.dds.models.repositories.ViandasRepository;
+import ar.edu.utn.frba.dds.models.repositories.colaborador.ColaboradorRepository;
+import ar.edu.utn.frba.dds.models.repositories.contribucion.DonacionViandasRepository;
 import ar.edu.utn.frba.dds.models.repositories.heladera.HeladerasRepository;
 import io.javalin.http.Context;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class DonacionViandaController {
+  public void registrarDonacionViandas(List<DonacionViandaInputDTO> dtos) {
+    // Recupero al colaborador
+    long idColaborador = dtos.stream().findFirst().get().getIdColaborador();
+    Colaborador colaborador = new ColaboradorRepository().findById(idColaborador).get();
+
+    // Recupero a la Heladera donde se depositaran las Viandas
+    long idHeladera = dtos.stream().findFirst().get().getIdHeladera();
+    Heladera heladeraDestino = new HeladerasRepository().findById(idHeladera).get();
+
+    System.out.println(
+            "Heladera antes de la Donación\n" +
+            "IdHeladera: " + heladeraDestino.getId() + "\n" +
+            "Cantidad de Viandas: " + new HeladerasRepository().getCantidadViandasDepositadas(heladeraDestino) + "\n" +
+            "Espacio Disponible: " + new HeladerasRepository().getCapacidadDisponible(heladeraDestino)
+    );
+
+    /* TODO: Nota: No se settea la Vianda a la Heladera hasta que no ingrese físicamente */
+    // Recorro los dtos para Instanciar las Viandas
+    Collection<Vianda> viandas = new ArrayList<>();
+    for (DonacionViandaInputDTO dto : dtos) {
+      // Instancio Vianda por Vianda
+      Vianda vianda = new Vianda(
+              dto.getDescripcion(),
+              dto.getFechaCaducidad(),
+              dto.getFechaDonacion(),
+              colaborador,
+              dto.getPesoEnGramos(),
+              dto.getCaloriasVianda()
+      );
+      viandas.add(vianda);
+    }
+    DonacionViandas donacionViandas = new DonacionViandas(
+            colaborador,
+            viandas,
+            heladeraDestino
+    );
+    new DonacionViandasRepository().insert(donacionViandas);
+    System.out.println(
+            "Heladera despues de la Donación\n" +
+                    "IdHeladera: " + heladeraDestino.getId() + "\n" +
+                    "Cantidad de Viandas: " + new HeladerasRepository().getCantidadViandasDepositadas(heladeraDestino) + "\n" +
+                    "Espacio Disponible: " + new HeladerasRepository().getCapacidadDisponible(heladeraDestino)
+    );
+    System.out.println(donacionViandas.toString());
+  }
+
   public void index(Context context) {
     Map<String, Object> model = context.attribute("model");
-    // Recupero las Heladeras para mostrar en el mapa.
-    List<Heladera> heladeras = new HeladerasRepository().findAll().toList();
-    model.put("heladeras", heladeras);
+
+    // Recupero heladeras
+    HeladerasRepository heladerasRepository = new HeladerasRepository();
+    List<Heladera> heladeras = heladerasRepository.findAll().toList();
+
+    // Tomo información de las heladeras.
+    List<Map<String, Object>> heladerasData = heladeras.stream().map(heladera -> {
+      Map<String, Object> heladeraData = new HashMap<>();
+      heladeraData.put("idHeladera", heladera.getId());
+      heladeraData.put("lat", heladera.getUbicacion().getLatitud());
+      heladeraData.put("long", heladera.getUbicacion().getLongitud());
+      heladeraData.put("nombre", heladera.getNombre());
+      heladeraData.put("capacidadDisponible", heladerasRepository.getCapacidadDisponible(heladera));
+      return heladeraData;
+    }).collect(Collectors.toList());
+
+    model.put("heladeras", heladerasData);
 
     context.render("contribuciones/donacion_vianda/donacion_vianda.hbs", model);
   }
 
-  //public void create(Context context) {}
+  public void create(Context context) {
+    // Recupero el ID de la Heladera donde se depositaran las viandas
+    System.out.println("Id Heladera: " + context.formParam("idHeladera"));
+    long idHeladera = Long.parseLong(context.formParam("idHeladera"));
+
+    // Recupero y Parseo la fecha en que se completo el Formulario
+    ZonedDateTime fechaDonacionParseada = ZonedDateTime.parse(context.formParam("fechaHora"));
+
+    // Creo viandas dependiendo de la cantidad de viandas que se ingresan en el formulario
+    List<DonacionViandaInputDTO> dtos = new ArrayList<>();
+    for (int i = 1; i <= Integer.parseInt(context.formParam("cantidad")); i++ ) {
+      // Parseo la Fecha que se trabaja con LocalDate desde el formulario
+      LocalDate fechaCaducidad = LocalDate.parse(context.formParam(String.format("fechaCaducidad_%s", i)));
+      ZonedDateTime fechaCaducidadParseada = fechaCaducidad.atStartOfDay(ZoneId.systemDefault());
+
+      // Comienzo con la creación del DTO
+      DonacionViandaInputDTO dto = new DonacionViandaInputDTO(
+        context.sessionAttribute("user_id"),
+        context.formParam(String.format("descripcionVianda_%s", i)),
+        fechaCaducidadParseada,
+        fechaDonacionParseada,
+        Double.parseDouble(context.formParam(String.format("pesoVianda_%s", i))),
+        Integer.parseInt(context.formParam(String.format("calorias_%s", i))),
+        idHeladera
+      );
+      dtos.add(dto);
+    }
+    registrarDonacionViandas(dtos);
+    context.redirect("/formas-colaboracion");
+  }
 }
