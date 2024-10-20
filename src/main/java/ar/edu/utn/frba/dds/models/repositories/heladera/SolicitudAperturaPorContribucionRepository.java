ea08377be4f1a8a79957809b9ce71c7e5484791e
febenitez04 <febenitez@frba.utn.edu.ar>
2024-10-20 09:31:19 -0300

    Query en SolicitudAperturaPorContribucionRepository cambiada

--- src/main/java/ar/edu/utn/frba/dds/models/repositories/heladera/SolicitudAperturaPorContribucionRepository.java
+++ src/main/java/ar/edu/utn/frba/dds/models/repositories/heladera/SolicitudAperturaPorContribucionRepository.java
@@ -1,6 +1,8 @@
 package ar.edu.utn.frba.dds.models.repositories.heladera;
 
+import ar.edu.utn.frba.dds.models.entities.contribucion.DonacionViandas;
 import ar.edu.utn.frba.dds.models.entities.contribucion.MovimientoViandas;
+import ar.edu.utn.frba.dds.models.entities.contribucion.RedistribucionViandas;
 import ar.edu.utn.frba.dds.models.entities.heladera.Heladera;
 import ar.edu.utn.frba.dds.models.entities.heladera.SolicitudAperturaPorContribucion;
 import ar.edu.utn.frba.dds.models.entities.heladera.SolicitudInvalidaException;
@@ -9,7 +11,9 @@ import ar.edu.utn.frba.dds.models.repositories.HibernateEntityManager;
 import javax.persistence.EntityManager;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Expression;
 import javax.persistence.criteria.Root;
+import javax.persistence.criteria.Subquery;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -38,14 +42,48 @@ public class SolicitudAperturaPorContribucionRepository extends HibernateEntityM
 
   public int getCantidadViandasPendientes(Heladera heladera) {
     EntityManager em = entityManager();
-    CriteriaBuilder cb = em.getCriteriaBuilder();
-    CriteriaQuery<MovimientoViandas> query = cb.createQuery(MovimientoViandas.class);
-    Root<MovimientoViandas> root = query.from(MovimientoViandas.class);
-    query.select(root).where(cb.equal(root.get("destino").get("id"), heladera.getId()));
-    return em.createQuery(query)
-            .getResultStream()
-            .mapToInt(solicitud -> solicitud.getViandas().size())
-            .sum();
+    try {
+      CriteriaBuilder cb = em.getCriteriaBuilder();
+      CriteriaQuery<Long> query = cb.createQuery(Long.class);
+
+      // Subconsulta para donaciones pendientes
+      Subquery<Long> donacionesSubquery = query.subquery(Long.class);
+      Root<DonacionViandas> donacionRoot = donacionesSubquery.from(DonacionViandas.class);
+
+      // Convertimos el resultado de cb.size() a Long
+      Expression<Long> sizeExpr = cb.toLong(cb.size(donacionRoot.get("viandas")));
+      donacionesSubquery.select(cb.sum(sizeExpr))
+              .where(cb.and(
+                      cb.equal(donacionRoot.get("destino"), heladera),
+                      cb.isNull(donacionRoot.get("fechaRealizada"))
+              ));
+
+      // Subconsulta para redistribuciones pendientes
+      Subquery<Long> redistribucionesSubquery = query.subquery(Long.class);
+      Root<RedistribucionViandas> redistribucionRoot = redistribucionesSubquery.from(RedistribucionViandas.class);
+
+      // Convertimos el resultado de cb.size() a Long
+      sizeExpr = cb.toLong(cb.size(redistribucionRoot.get("viandas")));
+      redistribucionesSubquery.select(cb.sum(sizeExpr))
+              .where(cb.and(
+                      cb.equal(redistribucionRoot.get("destino"), heladera),
+                      cb.isNull(redistribucionRoot.get("fechaRealizada"))
+              ));
+
+      // Consulta final sumando ambas subconsultas
+      query.select(cb.sum(cb.coalesce(donacionesSubquery, 0L),
+              cb.coalesce(redistribucionesSubquery, 0L)));
+
+      Long result = em.createQuery(query).getSingleResult();
+      return result != null ? result.intValue() : 0;
+    } catch (Exception e) {
+      e.printStackTrace();
+      return 0;
+    } finally {
+      if (em != null && em.isOpen()) {
+        em.close();
+      }
+    }
   }
 
   public void updateFechaUsada(Long id, boolean paraExtraccion, ZonedDateTime fechaUsada)
