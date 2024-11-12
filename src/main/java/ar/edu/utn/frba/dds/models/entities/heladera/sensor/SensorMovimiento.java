ebdc19090ca188a11cd0fab9075bc045c11ee545
febenitez04 <febenitez@frba.utn.edu.ar>
2024-11-12 18:56:33 -0300

    Sensores con anotaciones de hibernate

--- src/main/java/ar/edu/utn/frba/dds/models/entities/heladera/sensor/SensorMovimiento.java
+++ src/main/java/ar/edu/utn/frba/dds/models/entities/heladera/sensor/SensorMovimiento.java
@@ -4,21 +4,45 @@ import ar.edu.utn.frba.dds.dtos.input.sensores.SensorMovimientoInputDTO;
 import ar.edu.utn.frba.dds.models.entities.heladera.Heladera;
 import ar.edu.utn.frba.dds.sensores.ReceptorMovimiento;
 import ar.edu.utn.frba.dds.services.MqttBrokerService;
+import lombok.NonNull;
 import org.eclipse.paho.client.mqttv3.IMqttMessageListener;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Transient;
 import java.time.ZonedDateTime;
 
+@Entity
+@Table(name = "sensorMovimiento")
 public class SensorMovimiento implements IMqttMessageListener {
-  private ReceptorMovimiento receptorMovimiento;
-  private Heladera heladera;
+  @Id
+  @GeneratedValue
+  @Column(name = "id", nullable = false, updatable = false, unique = true)
+  @NonNull Long id;
+
+  @Transient
+  ReceptorMovimiento receptorMovimiento;
+
+  @OneToOne(targetEntity = Heladera.class)
+  @JoinColumn(name = "idHeladera", referencedColumnName = "id", nullable = false, updatable = false)
+  @MapsId
+  @NonNull Heladera heladera;
 
   public SensorMovimiento(ReceptorMovimiento receptorMovimiento, Heladera heladera) throws MqttException {
     this.receptorMovimiento = receptorMovimiento;
     this.heladera = heladera;
     MqttBrokerService.getInstancia().suscribir("heladera/movimiento", this);
   }
+
+  protected SensorMovimiento() {}
 
   public void recibirDatos(ZonedDateTime momentoEvento) {
     receptorMovimiento.evaluarReceptor(momentoEvento, heladera);
--- src/main/java/ar/edu/utn/frba/dds/models/entities/heladera/sensor/SensorTemperatura.java
+++ src/main/java/ar/edu/utn/frba/dds/models/entities/heladera/sensor/SensorTemperatura.java
@@ -4,21 +4,45 @@ import ar.edu.utn.frba.dds.dtos.input.sensores.SensorTemperaturaInputDTO;
 import ar.edu.utn.frba.dds.models.entities.heladera.Heladera;
 import ar.edu.utn.frba.dds.sensores.ReceptorTemperatura;
 import ar.edu.utn.frba.dds.services.MqttBrokerService;
+import lombok.NonNull;
 import org.eclipse.paho.client.mqttv3.IMqttMessageListener;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Transient;
 import java.time.ZonedDateTime;
 
+@Entity
+@Table(name = "sensorTemperatura")
 public class SensorTemperatura implements IMqttMessageListener {
-  final ReceptorTemperatura receptorTemperatura;
-  final Heladera heladera;
+  @Id
+  @GeneratedValue
+  @Column(name = "id", nullable = false, updatable = false, unique = true)
+  @NonNull Long id;
+
+  @Transient
+  ReceptorTemperatura receptorTemperatura;
+
+  @OneToOne(targetEntity = Heladera.class)
+  @JoinColumn(name = "idHeladera", referencedColumnName = "id", nullable = false, updatable = false)
+  @MapsId
+  @NonNull Heladera heladera;
 
   public SensorTemperatura(ReceptorTemperatura receptorTemperatura, Heladera heladera) throws MqttException {
     this.receptorTemperatura = receptorTemperatura;
     this.heladera = heladera;
     MqttBrokerService.getInstancia().suscribir("heladera/temperatura", this);
   }
+
+  protected SensorTemperatura() {}
 
   public void recibirDatos(double temperatura, ZonedDateTime momentoEvento) {
     this.receptorTemperatura.evaluarReceptor(temperatura, this.heladera, momentoEvento);
