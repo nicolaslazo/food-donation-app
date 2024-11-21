10af11f42282ee3abcd40da6ac7c8086acf58d64
nicolaslazo <45973144+nicolaslazo@users.noreply.github.com>
2024-11-21 09:49:23 -0300

    Funciones JS no usadas eliminadas

--- src/main/resources/public/js/tienda/ofrecer/script.js
+++ src/main/resources/public/js/tienda/ofrecer/script.js
@@ -1,39 +1,3 @@
-//FILTRAR//
-function filtrarPorCategoria() {
-    const categoriaSeleccionada = document.getElementById("category-filter").value;
-    const servicios = document.querySelectorAll(".service-item");
-    var noHayServicios = true;
-
-    servicios.forEach(servicio => {
-        const categoria = servicio.getAttribute("data-categoria");
-        if (categoriaSeleccionada === "todos" || categoria === categoriaSeleccionada) {
-            servicio.style.display = "flex";
-            noHayServicios = false;
-        } else {
-            servicio.style.display = "none";
-        }
-    });
-
-    var mensajeNoServicios = document.getElementById('no-services-message');
-    if (noHayServicios) {
-        mensajeNoServicios.style.display = 'block';
-    } else {
-        mensajeNoServicios.style.display = 'none';
-    }
-}
-
-function cerrarModal() {
-    document.getElementById('modal').style.display = 'none';
-
-    const pointsContainer = document.querySelector('.points-container');
-    pointsContainer.classList.remove('blur-background');
-    pointsContainer.style.pointerEvents = 'auto';
-    // Recargar la página actual
-    location.reload();
-}
-
-//AGREGAR RECOMPENSA
-
 function cerrarModalAgregarRecompensa() {
     document.getElementById('modal-agregar-servicio').style.display = 'none';
     const pointsContainer = document.querySelector('.points-container');
