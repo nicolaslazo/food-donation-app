aa215a0669287a9b3dd1331523a646c9cde25959
febenitez04 <febenitez@frba.utn.edu.ar>
2024-11-25 18:28:16 -0300

    Vista para ver reportes versión preliminar

--- src/main/java/ar/edu/utn/frba/dds/controllers/heladera/incidente/VisitaTecnicaController.java
+++ src/main/java/ar/edu/utn/frba/dds/controllers/heladera/incidente/VisitaTecnicaController.java
@@ -1,7 +1,11 @@
 package ar.edu.utn.frba.dds.controllers.heladera.incidente;
 
+import ar.edu.utn.frba.dds.models.entities.colaborador.Colaborador;
 import ar.edu.utn.frba.dds.models.entities.heladera.Heladera;
+import ar.edu.utn.frba.dds.models.entities.heladera.incidente.VisitaTecnica;
+import ar.edu.utn.frba.dds.models.repositories.colaborador.ColaboradorRepository;
 import ar.edu.utn.frba.dds.models.repositories.heladera.incidente.IncidenteRepository;
+import ar.edu.utn.frba.dds.models.repositories.heladera.incidente.VisitasTecnicasRepository;
 import io.javalin.http.Context;
 
 import java.util.List;
@@ -15,6 +19,18 @@ public class VisitaTecnicaController {
     model.put("heladeras", heladeras);
 
     context.render("incidentes/carga-visita-tecnica/visita-tecnica.hbs", model);
+  }
+
+  public void indexReporteTecnico(Context context) {
+    Map<String, Object> model = context.attribute("model");
+
+    Colaborador colaboradorResponsable = new ColaboradorRepository().findById(context.sessionAttribute("user_id")).get();
+
+    List<VisitaTecnica> reportesVisitasTecnicas = new VisitasTecnicasRepository().findAllVisitasTecnicas(colaboradorResponsable).toList();
+
+    model.put("visitasTecnicas", reportesVisitasTecnicas);
+
+    context.render("incidentes/visualizacion-visita-tecnica/visita-tecnica.hbs", model);
   }
 
   public void create(Context context) {}
